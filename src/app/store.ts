82c3380334175c214fa9014890cf5474cc82c966
Ben Peters <bencpeters@gmail.com>
2020-03-12 08:46:28 -0600

    Create React App auto-generated files

---
+++ src/app/store.ts
@@ -0,0 +1,11 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
