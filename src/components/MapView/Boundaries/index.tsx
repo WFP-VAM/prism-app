ac6a91265d64c13975bbc4bbbe3fa8104ba8c0e3
Eric Boucher <eric.p.boucher@gmail.com>
2020-04-16 18:21:53 -0700

    Get admin boundary data on click

--- src/components/MapView/Boundaries/index.tsx
+++ src/components/MapView/Boundaries/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import { get } from 'lodash';
 import { GeoJSONLayer } from 'react-mapbox-gl';
 import * as MapboxGL from 'mapbox-gl';
 import adminBoundaries from '../../../config/admin_boundaries.json';
-
-// console.log(adminBoundaries);
 
 const linePaint: MapboxGL.LinePaint = {
   'line-color': 'grey',
@@ -11,13 +10,26 @@ const linePaint: MapboxGL.LinePaint = {
   'line-opacity': 0.3,
 };
 
+/**
+ * To activate fillOnClick option, we "fill in"
+ * polygons with opacity 0.
+ */
+const fillPaint: MapboxGL.FillPaint = { 'fill-opacity': 0 };
+
+// Get admin data to process.
+function getAdminData(evt: any) {
+  console.log(get(evt.features[0], 'properties.ADM2_PCODE'));
+}
+
 function Boundaries() {
   return (
     <GeoJSONLayer
-      id="source-admin_boundaries"
-      type="geojson"
       data={adminBoundaries}
       linePaint={linePaint}
+      fillPaint={fillPaint}
+      fillOnClick={(evt: any) => {
+        getAdminData(evt);
+      }}
     />
   );
 }
