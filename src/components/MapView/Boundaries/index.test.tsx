3d785bbe5d51fca17f50b35c678290378dd17138
Eric Boucher <eric.p.boucher@gmail.com>
2020-04-27 15:27:14 -0700

    Use MapView to test Boundaries

--- src/components/MapView/Boundaries/index.test.tsx
+++ src/components/MapView/Boundaries/index.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Boundaries from '.';
+import MapView from '..';
+
+// Boundaries need to live in a "Map", so we
+// mock sub-components except for Boundaries
+jest.mock('../Layers', () => 'mock-Layers');
+jest.mock('../DateSelector', () => 'mock-DateSelector');
 
 test('renders as expected', () => {
-  const { container } = render(<Boundaries />);
+  const { container } = render(<MapView />);
   expect(container).toMatchSnapshot();
 });
