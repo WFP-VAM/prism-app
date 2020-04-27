55289333ee9fcdc1f7dd44e0d3d060795b7f288d
Eric Boucher <eric.p.boucher@gmail.com>
2020-04-27 14:45:21 -0700

    Fix date tests (#31)

--- src/components/MapView/DateSelector/index.test.tsx
+++ src/components/MapView/DateSelector/index.test.tsx
@@ -6,10 +6,16 @@ import DateSelector from '.';
 import { store } from '../../../context/store';
 
 test('renders as expected', () => {
+  const realDateNow = Date.now.bind(global.Date);
+  const dateNowStub = jest.fn(() => 1530518207007);
+  global.Date.now = dateNowStub;
+
   const { container } = render(
     <Provider store={store}>
       <DateSelector />
     </Provider>,
   );
   expect(container).toMatchSnapshot();
+
+  global.Date.now = realDateNow;
 });
