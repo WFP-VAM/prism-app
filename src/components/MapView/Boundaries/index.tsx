91b75645144e3343a2c50bf43e53f983a20e2fe5
Eric Boucher <eric.p.boucher@gmail.com>
2020-04-29 20:58:14 -0700

    Fix import

--- src/components/MapView/Boundaries/index.tsx
+++ src/components/MapView/Boundaries/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { get } from 'lodash';
 import { GeoJSONLayer } from 'react-mapbox-gl';
 import * as MapboxGL from 'mapbox-gl';
 import adminBoundaries from '../../../config/admin_boundaries.json';
