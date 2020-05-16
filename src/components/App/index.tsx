a0e97ef1747eebaf7e9c59ea9e875381214bb9c5
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-16 01:13:00 -0700

    Add Sentry

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -6,6 +7,13 @@ import NavBar from '../NavBar';
 import MapView from '../MapView';
 import NotFound from '../404Page';
 import muiTheme from '../../muiTheme';
+
+if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn:
+      'https://a3cc5f470dac4b50b288851a0390f686@o213971.ingest.sentry.io/5242966',
+  });
+}
 
 function App() {
   return (
