6b5c774f8f5629f2089459c2760866d643d3285a
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-16 17:15:16 -0700

    Add child data

--- src/config/baselines.tsx
+++ src/config/baselines.tsx
@@ -1,5 +1,6 @@
 import nsoDisabled from '../data/nso/number_of_disabled_persons.json';
 import nsoHerd from '../data/nso/NSO_Herd_Size_Admin1_LT_200.json';
+import nsoChild from '../data/nso/NSO_Child_U5_Admin2.json';
 // import nsoA from '../../../data/nso/number_of_disabled_persons.json';
 // import nsoB from '../../../data/nso/number_of_disabled_persons.json';
 // import nsoD from '../../../data/nso/number_of_disabled_persons.json';
@@ -7,13 +8,14 @@ import nsoHerd from '../data/nso/NSO_Herd_Size_Admin1_LT_200.json';
 const nsoDatasets = {
   nsoDisabled,
   nsoHerd,
+  nsoChild,
   // nsoPoverty: nsoPoverty,
   // nsoA: nsoA,
   // nsoB: nsoB,
   // nsoD: nsoD,
 };
 
-type DatasetKeys = 'nsoDisabled' | 'nsoDisabled' | 'nsoHerd';
+type DatasetKeys = 'nsoDisabled' | 'nsoChild' | 'nsoHerd';
 
 export function getNSOData(dataset: string | undefined) {
   if (dataset && dataset in nsoDatasets) {
