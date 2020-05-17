c6c940b94658803e80a76d09312f9de3c0526aaa
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-16 20:28:10 -0700

    Update baselines.tsx

--- src/config/baselines.tsx
+++ src/config/baselines.tsx
@@ -1,26 +1,30 @@
-import nsoDisabled from '../data/nso/number_of_disabled_persons.json';
-import nsoHerd from '../data/nso/NSO_Herd_Size_Admin1_LT_200.json';
+import nsoDisabled from '../data/nso/NSO_Disabled_Admin1_Total.json';
+import nsoHerd from '../data/nso/NSO_Herder_HHs_ths_Admin2.json';
 import nsoChild from '../data/nso/NSO_Child_U5_Admin2.json';
-
-// import nsoA from '../../../data/nso/number_of_disabled_persons.json';
-// import nsoB from '../../../data/nso/number_of_disabled_persons.json';
-// import nsoD from '../../../data/nso/number_of_disabled_persons.json';
+import nsoLivestock from '../data/nso/NSO_Livestock_Count_ths_Admin2.json';
+import nsoElderly from '../data/nso/NSO_Single_Elderly_Admin1_Total.json';
+import nsoPovery from '../data/nso/NSO_Poverty_Headcount_Admin1.json';
 
 const nsoDatasets = {
   nsoDisabled,
   nsoHerd,
   nsoChild,
-  // nsoPoverty: nsoPoverty,
-  // nsoA: nsoA,
-  // nsoB: nsoB,
-  // nsoD: nsoD,
+  nsoLivestock,
+  nsoElderly,
+  nsoPovery,
 };
 
-type DatasetKeys = 'nsoDisabled' | 'nsoChild' | 'nsoHerd';
+type DatasetKeys =
+  | 'nsoDisabled'
+  | 'nsoChild'
+  | 'nsoHerd'
+  | 'nsoLivestock'
+  | 'nsoElderly'
+  | 'nsoPovery';
 
 export function getNSOData(dataset: string | undefined) {
   if (dataset && dataset in nsoDatasets) {
     return nsoDatasets[dataset as DatasetKeys];
   }
-  return nsoDisabled;
+  return nsoPovery;
 }
