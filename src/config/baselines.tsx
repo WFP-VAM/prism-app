a805337421e31a7490a38fb15cf2b781d44155c2
Amit W <awadhwa@gmail.com>
2020-05-17 00:32:45 -0700

    additional fixes for baseline layers and legends

--- src/config/baselines.tsx
+++ src/config/baselines.tsx
@@ -1,9 +1,10 @@
 import nsoDisabled from '../data/nso/NSO_Disabled_Admin1_Total.json';
-import nsoHerd from '../data/nso/NSO_Herder_HHs_ths_Admin2.json';
+import nsoHerders from '../data/nso/NSO_Herder_HHs_Admin2.json';
+import nsoHerdsize from '../data/nso/NSO_Herd_Size_Admin1_LT_200.json';
 import nsoChild from '../data/nso/NSO_Child_U5_Admin2.json';
 import nsoLivestock from '../data/nso/NSO_Livestock_Count_ths_Admin2.json';
 import nsoElderly from '../data/nso/NSO_Single_Elderly_Admin1_Total.json';
-import nsoPovery from '../data/nso/NSO_Poverty_Headcount_Admin1.json';
+import nsoPoverty from '../data/nso/NSO_Poverty_Headcount_Admin1.json';
 
 export interface NsoData {
   TBL_ID: string;
@@ -26,24 +27,26 @@ export interface NsoDataset {
 
 const nsoDatasets = {
   nsoDisabled,
-  nsoHerd,
+  nsoHerders,
+  nsoHerdsize,
   nsoChild,
   nsoLivestock,
   nsoElderly,
-  nsoPovery,
+  nsoPoverty,
 };
 
 type DatasetKeys =
   | 'nsoDisabled'
   | 'nsoChild'
-  | 'nsoHerd'
+  | 'nsoHerders'
+  | 'nsoHerdsize'
   | 'nsoLivestock'
   | 'nsoElderly'
-  | 'nsoPovery';
+  | 'nsoPoverty';
 
 export function getNSOData(dataset: string | undefined) {
   if (dataset && dataset in nsoDatasets) {
     return nsoDatasets[dataset as DatasetKeys] as NsoDataset;
   }
-  return nsoPovery;
+  return nsoPoverty;
 }
