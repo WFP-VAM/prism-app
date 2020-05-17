5c65983dcc9ba5d8ff674eebaa74da9b9bdcf7b7
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-16 21:18:54 -0700

    Force NsoDataset type

--- src/config/baselines.tsx
+++ src/config/baselines.tsx
@@ -5,6 +5,25 @@ import nsoLivestock from '../data/nso/NSO_Livestock_Count_ths_Admin2.json';
 import nsoElderly from '../data/nso/NSO_Single_Elderly_Admin1_Total.json';
 import nsoPovery from '../data/nso/NSO_Poverty_Headcount_Admin1.json';
 
+export interface NsoData {
+  TBL_ID: string;
+  Period: string;
+  CODE?: string;
+  SCR_MN: string;
+  SCR_ENG: string;
+  CODE1?: string;
+  CODE2?: null;
+  SCR_MN1: string;
+  SCR_ENG1: string;
+  SCR_MN2?: null;
+  SCR_ENG2?: null;
+  DTVAL_CO: string;
+}
+
+export interface NsoDataset {
+  DataList: NsoData[];
+}
+
 const nsoDatasets = {
   nsoDisabled,
   nsoHerd,
@@ -24,7 +43,7 @@ type DatasetKeys =
 
 export function getNSOData(dataset: string | undefined) {
   if (dataset && dataset in nsoDatasets) {
-    return nsoDatasets[dataset as DatasetKeys];
+    return nsoDatasets[dataset as DatasetKeys] as NsoDataset;
   }
   return nsoPovery;
 }
