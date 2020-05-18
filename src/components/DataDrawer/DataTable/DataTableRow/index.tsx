0fcbec9737ea9bd092b6a1dd14b04cad0fa551c3
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-18 15:29:13 -0700

    Fix index error

--- src/components/DataDrawer/DataTable/DataTableRow/index.tsx
+++ src/components/DataDrawer/DataTable/DataTableRow/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { TableRow, TableCell } from '@material-ui/core';
 
 export interface TableRowProps {
-  key: string;
   className?: string;
   rowData: any;
 }
 
-const DataTableRow = ({ className, rowData, key }: TableRowProps) => (
-  <TableRow key={key}>
+const DataTableRow = ({ className, rowData }: TableRowProps) => (
+  <TableRow>
     {Object.entries(rowData).map(([cellKey, value]) => (
       <TableCell className={className} key={cellKey}>
         {' '}
