747f7fdf82ca6d7a0e585395389f6639da5be153
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-19 14:28:41 -0700

    Move useEffect to the reducer

--- src/context/tableStateSlice.ts
+++ src/context/tableStateSlice.ts
@@ -1,4 +1,5 @@
 import { Map } from 'immutable';
+import Papa from 'papaparse';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { RootState } from './store';
@@ -8,15 +9,31 @@ interface TableState extends Map<string, any> {}
 
 const initialState: TableState = Map({
   table: {} as TableType,
+  tableJson: [] as any[],
   isShowing: false,
 });
+
+// parse the csv, but only when we get a new table to parse
+function setTableState(state: any, table: TableType) {
+  Papa.parse(process.env.PUBLIC_URL + table.table, {
+    header: true,
+    download: true,
+    complete: results => {
+      console.log(results.data);
+      state
+        .set('table', table)
+        .set('isShowing', true)
+        .set('tableJson', results.data);
+    },
+  });
+}
 
 export const tableStateSlice = createSlice({
   name: 'tableState',
   initialState,
   reducers: {
     setTable: (state, { payload }: PayloadAction<TableType>) =>
-      state.set('table', payload).set('isShowing', true),
+      setTableState(state, payload),
     hideTable: state => state.set('isShowing', false),
   },
 });
@@ -24,6 +41,10 @@ export const tableStateSlice = createSlice({
 // TODO: memoize this to prevent repeat loading and parsing of CSV
 export const getCurrTable = (state: RootState): TableType => {
   return state.tableState.get('table') as TableType;
+};
+
+export const getCurrTableJson = (state: RootState): any[] => {
+  return state.tableState.get('tableJson') as any[];
 };
 
 // prevent trying to load and display table on start of app
