b89ed9c228585cb35f6455da8d29b94b55d6ca16
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-12 17:42:19 -0700

    Update main.py

--- api-flask/app/main.py
+++ api-flask/app/main.py
@@ -27,13 +27,14 @@ cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @timed
 @cache.memoize(50)
-def _calculate_stats(zones, geotiff, stats, prefix, geojson_out):
+def _calculate_stats(zones, geotiff, stats, prefix, group_by, geojson_out):
     """Calculate stats."""
     return calculate_stats(
         zones,
         geotiff,
         stats=stats,
         prefix=prefix,
+        group_by=group_by,
         geojson_out=geojson_out
     )
 
@@ -42,33 +43,43 @@ def _calculate_stats(zones, geotiff, stats, prefix, geojson_out):
 @timed
 def stats():
     """Return zonal statistics."""
-    geotiff_url = 'https://mongolia.sibelius-datacube.org:5000/?service=WCS&'\
-        'request=GetCoverage&version=1.0.0&coverage=ModisAnomaly&'\
-        'crs=EPSG%3A4326&bbox=86.5%2C36.7%2C119.7%2C55.3&width=1196&'\
-        'height=672&format=GeoTIFF&time=2020-03-01'
-
-    if request.method != 'POST':
-        geotiff = cache_file(
-            prefix='test',
-            url=geotiff_url
-        )
-        zones = './admin_boundaries.json'
-
     if request.method == 'POST':
-        geotiff_url = request.form['geotiff_url']
-        zones_url = request.form['zones_url']
+        geotiff_url = request.form.get('geotiff_url', None)
+        zones_url = request.form.get('zones_url', None)
+        if not (geotiff_url and zones_url):
+            raise Exception('geotiff_url and zones_url are both required.')
+
+        geojson_out = request.form.get('geojson_out', False)
+        group_by = request.form.get('group_by', None)
+
         geotiff = cache_file(
             prefix='test',
             url=geotiff_url
         )
         zones = zones_url
 
+    # The GET endpoint is used for testing purposes
+    if request.method == 'GET':
+        geotiff_url = 'https://mongolia.sibelius-datacube.org:5000/?service=WCS&'\
+            'request=GetCoverage&version=1.0.0&coverage=ModisAnomaly&'\
+            'crs=EPSG%3A4326&bbox=86.5%2C36.7%2C119.7%2C55.3&width=1196&'\
+            'height=672&format=GeoTIFF&time=2020-03-01'
+
+        geotiff = cache_file(
+            prefix='test',
+            url=geotiff_url
+        )
+        zones = './admin_boundaries.json'
+        geojson_out = False
+        group_by = None
+
     features = _calculate_stats(
         zones,
         geotiff,
         stats=['min', 'max', 'mean', 'median', 'sum', 'std'],
         prefix='stats_',
-        geojson_out=True
+        group_by=group_by,
+        geojson_out=geojson_out
     )
     return jsonify(features)
 
