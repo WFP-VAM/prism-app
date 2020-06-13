7ab40b28bfebb7bbefcfa1e489b6e3315cf01c07
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-12 17:31:19 -0700

    Add group_by capabilities

--- api-flask/app/tests/test_calculate.py
+++ api-flask/app/tests/test_calculate.py
@@ -14,6 +14,19 @@ def test_calculate_stats_json_output():
     assert True
 
 
+def test_calculate_stats_with_group_by():
+    """
+    Test that calculate_stats given a geojson input and
+    """
+
+    zones = '/app/tests/small_admin_boundaries.json'
+    geotiff = '/app/tests/raster_sample.tif'
+    features = calculate_stats(
+        zones, geotiff, group_by='ADM1_PCODE', geojson_out=False)
+    assert len(features) == 4
+    assert True
+
+
 def test_calculate_stats_geojson_output():
     """
     Test that calculate_stats given a geojson input and
--- api-flask/app/zonal_stats.py
+++ api-flask/app/zonal_stats.py
@@ -1,8 +1,12 @@
 """Calulate zonal statistics and return a json or a geojson."""
-import json
 import logging
+from collections import defaultdict
+from json import dump, load
 
 from rasterstats import zonal_stats
+
+from shapely.geometry import mapping, shape
+from shapely.ops import cascaded_union
 
 from timer import timed
 
@@ -14,12 +18,51 @@ DEFAULT_STATS = ['min', 'max', 'mean', 'median']
 
 def _extract_features_properties(zones):
     with open(zones) as json_file:
-        geojson_data = json.load(json_file)
+        geojson_data = load(json_file)
     features = geojson_data.get('features', [])
     return [feature['properties'] for feature in features]
 
 
-@timed
+def _group_zones(zones, group_by):
+    """Group zones by a key id and merge polygons."""
+    with open(zones) as json_file:
+        geojson_data = load(json_file)
+
+    features = geojson_data.get('features', [])
+
+    grouped_polygons = defaultdict(list)
+
+    for zone_feature in features:
+        grouped_polygons[zone_feature['properties'][group_by]].append(
+            shape(zone_feature['geometry'])
+        )
+
+    new_features = []
+    for group_id, polygons in grouped_polygons.items():
+        new_geometry = mapping(cascaded_union(polygons))
+
+        new_features.append(
+            dict(
+                type='Feature',
+                id=group_id,
+                properties=dict([(group_by, group_id)]),
+                geometry=dict(
+                    type=new_geometry['type'],
+                    coordinates=new_geometry['coordinates'])
+            )
+        )
+
+    outjson = dict(type='FeatureCollection', features=new_features)
+
+    output_file = '{zones}.{group_by}'.format(zones=zones, group_by=group_by)
+
+    with open(output_file, 'w') as outfile:
+        dump(outjson, outfile)
+
+    return output_file
+
+
+@ timed
 def calculate_stats(
     zones,
     geotiff,
@@ -29,6 +72,9 @@ def calculate_stats(
     geojson_out=False
 ):
     """Calculate stats."""
+    if group_by:
+        zones = _group_zones(zones, group_by)
+
     stats = zonal_stats(
         zones,
         geotiff,
