4c1dd9b129276eab5baa91a3e2f79ddf453487f8
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-15 14:08:34 -0700

    Update test docstrings

--- api-flask/app/tests/test_calculate.py
+++ api-flask/app/tests/test_calculate.py
@@ -3,9 +3,7 @@ from app.zonal_stats import calculate_stats
 
 
 def test_calculate_stats_json_output():
-    """
-    Test that calculate_stats given a geojson input and
-    """
+    """ Test calculate_stats with geojson_out=False."""
 
     zones = '/app/tests/small_admin_boundaries.json'
     geotiff = '/app/tests/raster_sample.tif'
@@ -14,27 +12,23 @@ def test_calculate_stats_json_output():
     assert True
 
 
-def test_calculate_stats_with_group_by():
-    """
-    Test that calculate_stats given a geojson input and
-    """
-
-    zones = '/app/tests/small_admin_boundaries.json'
-    geotiff = '/app/tests/raster_sample.tif'
-    features = calculate_stats(
-        zones, geotiff, group_by='ADM1_PCODE', geojson_out=False)
-    assert len(features) == 4
-    assert True
-
-
 def test_calculate_stats_geojson_output():
-    """
-    Test that calculate_stats given a geojson input and
-    """
+    """ Test calculate_stats with geojson_out=True."""
 
     zones = '/app/tests/small_admin_boundaries.json'
     geotiff = '/app/tests/raster_sample.tif'
     features = calculate_stats(zones, geotiff, geojson_out=True)
     assert len(features) == 26
     assert features[0]['type'] == 'Feature'
+    assert True
+
+
+def test_calculate_stats_with_group_by():
+    """ Test calculate_stats with a group_by argument."""
+
+    zones = '/app/tests/small_admin_boundaries.json'
+    geotiff = '/app/tests/raster_sample.tif'
+    features = calculate_stats(
+        zones, geotiff, group_by='ADM1_PCODE', geojson_out=False)
+    assert len(features) == 4
     assert True
