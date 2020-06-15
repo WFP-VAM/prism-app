8ddf5f2637cde2d0412715625f9e1a30bd165714
Ryan Samarakoon <ryan2002.rs@gmail.com>
2020-06-15 22:26:52 +1000

    :white_check_mark: Fixed Failing MapView Test

--- src/components/MapView/index.test.tsx
+++ src/components/MapView/index.test.tsx
@@ -5,8 +5,8 @@ import { render } from '@testing-library/react';
 import MapView from '.';
 import { store } from '../../context/store';
 
-jest.mock('../../context/layers/layer-data', () => 'mock-layer-data');
 jest.mock('./Layers/WMSLayer', () => 'mock-WMSLayer');
+jest.mock('./Layers/ImpactLayer', () => 'mock-ImpactLayer');
 jest.mock('./Layers/NSOLayer', () => 'mock-NSOLayer');
 jest.mock('./Layers/BoundaryLayer', () => 'mock-BoundaryLayer');
 
