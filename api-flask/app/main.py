a67b64f41c00dbf6c0cf031f0daa9274b04c5c0c
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-16 16:21:07 -0700

    cors test

--- api-flask/app/main.py
+++ api-flask/app/main.py
@@ -18,7 +18,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 # For more configuration options, check out the documentation
 # Caching durations are in seconds.
