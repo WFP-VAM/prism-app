1f4c8247af898109d734790336ada9fb6bb297a0
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-17 11:41:01 -0700

    No need for cross_origin

--- api-flask/app/main.py
+++ api-flask/app/main.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, request
 
 from flask_caching import Cache
 
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 from timer import timed
 
@@ -41,7 +41,6 @@ def _calculate_stats(zones, geotiff, stats, prefix, group_by, geojson_out):
 
 
 @app.route('/stats', methods=['POST'])
-@cross_origin()
 @timed
 def stats():
     """Return zonal statistics."""
@@ -79,7 +78,6 @@ def stats():
 
 
 @app.route('/demo', methods=['GET'])
-@cross_origin()
 @timed
 def stats_demo():
     """Return examples of zonal statistics."""
