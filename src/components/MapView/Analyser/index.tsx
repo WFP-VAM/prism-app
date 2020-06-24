f5c3df2ea9ee1098d6b1d21f07dd96294185ed6c
Ryan Samarakoon <ryan2002.rs@gmail.com>
2020-06-24 14:59:22 +1000

    :lipstick: Improved UI for analysis menu

--- src/components/MapView/Analyser/index.tsx
+++ src/components/MapView/Analyser/index.tsx
@@ -3,21 +3,31 @@ import {
   Button,
   createStyles,
   Theme,
+  Typography,
   withStyles,
   WithStyles,
 } from '@material-ui/core';
+import { Assessment } from '@material-ui/icons';
 
 function Analyser({ classes }: AnalyserProps) {
   const [open, setOpen] = React.useState(false);
 
   return (
     <div className={classes.container}>
-      <Button onClick={() => setOpen(!open)}>Run analysis</Button>
-      {open && (
-        <>
-          <p>This is an analyser</p>
-        </>
-      )}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setOpen(!open)}
+      >
+        <Assessment style={{ marginRight: '10px' }} />
+        <Typography variant="body2">Run Analysis</Typography>
+      </Button>
+      <div
+        className={classes.analyserMenu}
+        style={{ width: open ? 500 : 0, padding: open ? 50 : 0 }}
+      >
+        This is Analyser Menu
+      </div>
     </div>
   );
 }
@@ -27,9 +37,21 @@ const styles = (theme: Theme) =>
     container: {
       zIndex: theme.zIndex.drawer,
       position: 'absolute',
-      top: 24,
-      left: 24,
+      top: -2,
+      left: -2,
       textAlign: 'left',
+    },
+    analyserMenu: {
+      backgroundColor: '#5A686C',
+      maxWidth: '100vw',
+      color: 'white',
+      overflowX: 'hidden',
+      transition: 'all 0.5s ease-in-out',
+      whiteSpace: 'nowrap',
+      borderTopRightRadius: '10px',
+      borderBottomRightRadius: '10px',
+      height: '600px',
+      maxHeight: '90vh',
     },
   });
 
