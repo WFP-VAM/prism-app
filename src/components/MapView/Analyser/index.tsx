be14c6b10ac4831a708948727cc798f3ff1cef00
Laveesh Gupta <loveeshgoyal@gmail.com>
2020-06-25 13:19:33 +0530

    WIP: listing the layers option to configure for analysis

--- src/components/MapView/Analyser/index.tsx
+++ src/components/MapView/Analyser/index.tsx
@@ -6,7 +6,11 @@ import {
   Typography,
   withStyles,
   WithStyles,
+  RadioGroup,
   Radio,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
 } from '@material-ui/core';
 import { Assessment, ArrowDropDown } from '@material-ui/icons';
 import { extent as calculateExtentFromGeoJSON } from 'geojson-bounds';
@@ -51,11 +55,12 @@ function Analyser({ classes }: AnalyserProps) {
   console.log(adminBoundariesExtent);
 
   return (
-    <div className={classes.container}>
+    <div className={classes.analyser}>
       <Button
         variant="contained"
         color="primary"
         onClick={() => setOpen(!open)}
+        style={{ display: open ? 'none' : 'inline-flex' }}
       >
         <Assessment style={{ marginRight: '10px' }} />
         <Typography variant="body2">Run Analysis</Typography>
@@ -79,10 +84,161 @@ function Analyser({ classes }: AnalyserProps) {
 
       <div
         className={classes.analyserMenu}
-        style={{ width: open ? 500 : 0, padding: open ? 50 : 0 }}
+        style={{ width: open ? '40vw' : 0, padding: open ? 10 : 0 }}
       >
-        {baselineLayers.reduce((str, layer) => `${str + layer.title}\n`, '')}
-        {hazardLayers.reduce((str, layer) => `${str + layer.title}\n`, '')}
+        <FormControl component="fieldset">
+          <RadioGroup>
+            <FormControlLabel
+              value="new"
+              control={
+                <Radio className={classes.analyserOptions} size="small" />
+              }
+              label="Create a new analysis"
+            />
+            <FormControlLabel
+              value="pre-configure"
+              control={
+                <Radio className={classes.analyserOptions} size="small" />
+              }
+              label="Run a pre-configured analysis"
+            />
+            <FormControlLabel
+              value="generate"
+              control={
+                <Radio className={classes.analyserOptions} size="small" />
+              }
+              label="Generate spatial statistics"
+            />
+          </RadioGroup>
+        </FormControl>
+        <div className={classes.newAnalyserContainer}>
+          <div>
+            <Typography>Step 1 - Choose a hazard Layer</Typography>
+            <div>
+              <FormControl component="div">
+                <FormLabel component="legend">Drought Indication</FormLabel>
+                <RadioGroup>
+                  <FormControlLabel
+                    value="new"
+                    control={
+                      <Radio className={classes.radioOptions} size="small" />
+                    }
+                    label="Pasture Anomaly"
+                  />
+                  <FormControlLabel
+                    value="pre-configure"
+                    control={
+                      <Radio className={classes.radioOptions} size="small" />
+                    }
+                    label="Vegetation Index (NDVI)"
+                  />
+                  <FormControlLabel
+                    value="generate"
+                    control={
+                      <Radio className={classes.radioOptions} size="small" />
+                    }
+                    label="Agricultural Drought (VHI)"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </div>
+            <div>
+              <FormControl component="div">
+                <FormLabel component="legend">Drought Indication</FormLabel>
+                <RadioGroup>
+                  <FormControlLabel
+                    value="new"
+                    control={
+                      <Radio className={classes.radioOptions} size="small" />
+                    }
+                    label="Pasture Anomaly"
+                  />
+                  <FormControlLabel
+                    value="pre-configure"
+                    control={
+                      <Radio className={classes.radioOptions} size="small" />
+                    }
+                    label="Vegetation Index (NDVI)"
+                  />
+                  <FormControlLabel
+                    value="generate"
+                    control={
+                      <Radio className={classes.radioOptions} size="small" />
+                    }
+                    label="Agricultural Drought (VHI)"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </div>
+          </div>
+          <div>
+            <Typography>Step 1 - Choose a hazard Layer</Typography>
+            <FormControl component="div">
+              <FormLabel component="legend">Drought Indication</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="new"
+                  control={
+                    <Radio className={classes.radioOptions} size="small" />
+                  }
+                  label="Pasture Anomaly"
+                />
+                <FormControlLabel
+                  value="pre-configure"
+                  control={
+                    <Radio className={classes.radioOptions} size="small" />
+                  }
+                  label="Vegetation Index (NDVI)"
+                />
+                <FormControlLabel
+                  value="generate"
+                  control={
+                    <Radio className={classes.radioOptions} size="small" />
+                  }
+                  label="Agricultural Drought (VHI)"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+          <div>
+            <Typography>Step 1 - Choose a hazard Layer</Typography>
+            <FormControl component="div">
+              <FormLabel component="legend">Drought Indication</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="new"
+                  control={
+                    <Radio className={classes.radioOptions} size="small" />
+                  }
+                  label="Pasture Anomaly"
+                />
+                <FormControlLabel
+                  value="pre-configure"
+                  control={
+                    <Radio className={classes.radioOptions} size="small" />
+                  }
+                  label="Vegetation Index (NDVI)"
+                />
+                <FormControlLabel
+                  value="generate"
+                  control={
+                    <Radio className={classes.radioOptions} size="small" />
+                  }
+                  label="Agricultural Drought (VHI)"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+        </div>
+        <Button>
+          <Typography variant="body2">Run Analysis</Typography>
+        </Button>
+        <Button>
+          <Typography variant="body2">Show Result</Typography>
+        </Button>
+        <Button>
+          <Typography variant="body2">Download</Typography>
+        </Button>
       </div>
     </div>
   );
@@ -112,7 +268,7 @@ async function submitAnaysisRequest(
 
 const styles = (theme: Theme) =>
   createStyles({
-    container: {
+    analyser: {
       zIndex: theme.zIndex.drawer,
       position: 'absolute',
       top: 2,
@@ -124,7 +280,7 @@ const styles = (theme: Theme) =>
       maxWidth: '100vw',
       color: 'white',
       overflowX: 'hidden',
-      transition: 'all 0.5s ease-in-out',
+      // transition: 'width 0.5s ease-in-out',
       whiteSpace: 'nowrap',
       borderTopRightRadius: '10px',
       borderBottomRightRadius: '10px',
@@ -134,6 +290,18 @@ const styles = (theme: Theme) =>
     analyserButton: {
       height: '36px',
       'margin-left': '3px',
+    },
+    analyserOptions: {
+      color: 'white',
+      padding: '2px 5px',
+    },
+    newAnalyserContainer: {
+      padding: '5px',
+    },
+    radioOptions: {
+      color: 'white',
+      paddingTop: '2px',
+      paddingBottom: '2px',
     },
   });
 
