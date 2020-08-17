4e20d352d821981fa70ca6bc1dcbd28dd8928e16
Ryab <ryan2002.rs@gmail.com>
2020-08-17 11:47:21 +1000

    :label: Improve typing and documentation

--- src/context/layers/groundstation.ts
+++ src/context/layers/groundstation.ts
@@ -1,6 +1,6 @@
 import GeoJSON from 'geojson';
 import moment from 'moment';
-import type { LayerDataParams, LazyLoader } from './layer-data';
+import type { LazyLoader } from './layer-data';
 import { GroundstationLayerProps } from '../../config/types';
 
 declare module 'geojson' {
@@ -23,7 +23,7 @@ export type GroundstationLayerData = {
 export const fetchGroundstationData: LazyLoader<GroundstationLayerProps> = () => async ({
   date,
   layer,
-}: LayerDataParams<GroundstationLayerProps>) => {
+}) => {
   // This function fetches groundstation data from the API.
   // If this endpoint is not available or we run into an error,
   // we should get the data from the local public file in layer.fallbackData
--- src/context/layers/layer-data.ts
+++ src/context/layers/layer-data.ts
@@ -60,7 +60,7 @@ type LayerLoaders = {
 
 // Less restrictive handler type since Typescript doesn't support nested union discrimination
 export type LazyLoader<T extends LayerType> = (
-  recursive: LoadLayerDataFuncType,
+  recursive: LoadLayerDataFuncType, // just in case a loader wants to load another layer, we pass the function recursively to prevent import cycles.
 ) => (
   params: LayerDataParams<T>,
   api: ThunkApi,
