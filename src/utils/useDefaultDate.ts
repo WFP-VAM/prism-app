4f1d935589341012df838494b54650fd0e195c96
Ryan Samarakoon <ryan2002.rs@gmail.com>
2020-08-27 22:42:46 +1000

    :bug: Fixed useDefaultDate, only tested in +GMT Timezone

--- src/utils/useDefaultDate.ts
+++ src/utils/useDefaultDate.ts
@@ -1,8 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { AvailableDates } from '../config/types';
 import { availableDatesSelector } from '../context/serverStateSlice';
 import { dateRangeSelector } from '../context/mapStateSlice/selectors';
 import { updateDateRange } from '../context/mapStateSlice';
+import { USER_DATE_OFFSET } from '../components/MapView/DateSelector';
 /**
  * A hook designed to automatically load the default date of a layer if the user doesn't select one.
  * returns either the user selected date or the default date, dispatching it to the date picker beforehand. Can also return undefined if no default date is available.
@@ -17,11 +19,17 @@ export function useDefaultDate(
   const possibleDates = useSelector(availableDatesSelector)[
     availableDatesLookupKey
   ];
-  const defaultDate: number | undefined =
-    possibleDates?.[possibleDates?.length - 1];
 
-  if (!selectedDate && defaultDate) {
-    dispatch(updateDateRange({ startDate: defaultDate }));
-  }
+  const defaultDate: number | undefined =
+    possibleDates?.[possibleDates?.length - 1] + USER_DATE_OFFSET;
+
+  // React doesn't allow updating other components within another component
+  // useEffect removes this error and updates DateSelector correctly in the lifecycle.
+  useEffect(() => {
+    if (!selectedDate && defaultDate) {
+      dispatch(updateDateRange({ startDate: defaultDate }));
+    }
+  }, [defaultDate, dispatch, selectedDate]);
+
   return selectedDate || defaultDate;
 }
