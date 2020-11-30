b6f176980f74df8a6e630c32bc8710898d7e22d2
muhammad <h_enzi@yahoo.co.id>
2020-11-30 16:22:03 +0700

    change download button to dropdown

--- src/components/MapView/Download/index.tsx
+++ src/components/MapView/Download/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   Button,
   createStyles,
@@ -8,22 +8,66 @@ import {
   DialogTitle,
   Grid,
   Hidden,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
   Theme,
   Typography,
   WithStyles,
   withStyles,
 } from '@material-ui/core';
+import Menu, { MenuProps } from '@material-ui/core/Menu';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFileExport,
+  faCaretDown,
+  faImage,
+} from '@fortawesome/free-solid-svg-icons';
 
 import { useSelector } from 'react-redux';
 import { mapSelector } from '../../../context/mapStateSlice/selectors';
 
+const ExportMenu = withStyles((theme: Theme) => ({
+  paper: {
+    border: '1px solid #d3d4d5',
+    backgroundColor: theme.palette.primary.main,
+  },
+}))((props: MenuProps) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const ExportMenuItem = withStyles((theme: Theme) => ({
+  root: {
+    color: theme.palette.common.white,
+  },
+}))(MenuItem);
+
 function Download({ classes }: DownloadProps) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
   const selectedMap = useSelector(mapSelector);
   const previewRef = useRef<HTMLCanvasElement>(null);
-  const [open, setOpen] = React.useState(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
     if (open && selectedMap) {
@@ -36,7 +80,7 @@ function Download({ classes }: DownloadProps) {
     }
   });
 
-  const handleClose = () => {
+  const modalClose = () => {
     setOpen(false);
   };
 
@@ -52,19 +96,38 @@ function Download({ classes }: DownloadProps) {
 
   return (
     <Grid item>
-      <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
-        <FontAwesomeIcon style={{ fontSize: '1em' }} icon={faDownload} />
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        <FontAwesomeIcon style={{ fontSize: '1em' }} icon={faFileExport} />
         <Hidden smDown>
           <Typography className={classes.label} variant="body2">
-            Download
+            Export
           </Typography>
         </Hidden>
+        <FontAwesomeIcon icon={faCaretDown} style={{ marginLeft: '10px' }} />
       </Button>
+      <ExportMenu
+        id="export-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <ExportMenuItem>
+          <ListItemIcon onClick={() => setOpen(true)}>
+            <FontAwesomeIcon
+              color="white"
+              style={{ fontSize: '1em' }}
+              icon={faImage}
+            />
+          </ListItemIcon>
+          <ListItemText primary="PNG" />
+        </ExportMenuItem>
+      </ExportMenu>
       <Dialog
         maxWidth="xl"
         open={open}
         keepMounted
-        onClose={handleClose}
+        onClose={modalClose}
         aria-labelledby="dialog-preview"
       >
         <DialogTitle className={classes.title} id="dialog-preview">
@@ -74,7 +137,7 @@ function Download({ classes }: DownloadProps) {
           <canvas ref={previewRef} />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={modalClose} color="primary">
             Cancel
           </Button>
           <Button variant="contained" onClick={download} color="primary">
