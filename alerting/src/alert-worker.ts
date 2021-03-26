7a109f80d2c6d2f4ea5e8e1229d3e995fdbe9baa
Eric Boucher <eric.p.boucher@gmail.com>
2021-03-25 19:27:43 -0500

    Exit early if layer has already been analyzed on this date

--- alerting/src/alert-worker.ts
+++ alerting/src/alert-worker.ts
@@ -1,7 +1,7 @@
 import { createConnection } from 'typeorm';
 import { Alert } from './entities/alerts.entity';
 import { calculateBoundsForAlert } from './utils/analysis-utils';
-import { getWMSCapabilities } from './utils/server-utils';
+import { getWCSCoverage, getWMSCapabilities } from './utils/server-utils';
 
 async function run() {
   const connection = await createConnection();
@@ -10,12 +10,28 @@ async function run() {
   const alerts = await alertRepository.find();
   await Promise.all(
     alerts.map(async (alert) => {
-      const { baseUrl, serverLayerName } = alert.alertConfig;
-      const availableDates = await getWMSCapabilities(`${baseUrl}/wms`);
+      const { baseUrl, serverLayerName, type } = alert.alertConfig;
+      const { lastTriggered } = alert;
+      const availableDates =
+        type === 'wms'
+          ? await getWMSCapabilities(`${baseUrl}/wms`)
+          : await getWCSCoverage(`${baseUrl}`);
       const layerAvailableDates = availableDates[serverLayerName];
       const maxDate = new Date(Math.max(...layerAvailableDates));
-      console.log(maxDate);
+
+      if (lastTriggered >= maxDate) {
+        return;
+      }
+
       const alertMessage = await calculateBoundsForAlert(maxDate, alert);
+      if (!alertMessage) {
+        return;
+      }
+      console.log(
+        `Your alert '${alert.alertName}' was triggered on ${maxDate}`,
+      );
+      // TODO - Send an email
+      // TODO - update
       console.log(alertMessage);
     }),
   );
