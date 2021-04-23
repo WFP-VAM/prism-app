25797469b07a850be5b2893b6211c6a6e4eb5587
Eric Boucher <eric.p.boucher@gmail.com>
2021-04-23 11:15:03 -0700

    Update alert-worker.ts

--- alerting/src/alert-worker.ts
+++ alerting/src/alert-worker.ts
@@ -1,37 +1,28 @@
 import { isNaN } from 'lodash';
-import { createConnection } from 'typeorm';
+import { createConnection, Repository } from 'typeorm';
 import { Alert } from './entities/alerts.entity';
 import { calculateBoundsForAlert } from './utils/analysis-utils';
 import { sendEmail } from './utils/email';
 import { getWCSCoverage, getWMSCapabilities } from './utils/server-utils';
 
-async function run() {
-  const connection = await createConnection();
-  const alertRepository = connection.getRepository(Alert);
+async function processAlert(alert: Alert, alertRepository: Repository<Alert>) {
+  const { baseUrl, serverLayerName, type } = alert.alertConfig;
+  const { id, alertName, email, lastTriggered, prismUrl } = alert;
+  const availableDates =
+    type === 'wms'
+      ? await getWMSCapabilities(`${baseUrl}/wms`)
+      : await getWCSCoverage(`${baseUrl}`);
+  const layerAvailableDates = availableDates[serverLayerName];
+  const maxDate = new Date(Math.max(...(layerAvailableDates || [])));
 
-  const alerts = await alertRepository.find();
-  await Promise.all(
-    alerts.map(async (alert) => {
-      const { baseUrl, serverLayerName, type } = alert.alertConfig;
-      const { id, alertName, email, lastTriggered, prismUrl } = alert;
-      const availableDates =
-        type === 'wms'
-          ? await getWMSCapabilities(`${baseUrl}/wms`)
-          : await getWCSCoverage(`${baseUrl}`);
-      const layerAvailableDates = availableDates[serverLayerName];
-      const maxDate = new Date(Math.max(...(layerAvailableDates || [])));
+  if (isNaN(maxDate.getTime()) || (lastTriggered && lastTriggered >= maxDate)) {
+    return;
+  }
 
-      if (
-        isNaN(maxDate.getTime()) ||
-        (lastTriggered && lastTriggered >= maxDate)
-      ) {
-        return;
-      }
+  const alertMessage = await calculateBoundsForAlert(maxDate, alert);
 
-      const alertMessage = await calculateBoundsForAlert(maxDate, alert);
-
-      if (alertMessage) {
-        const emailMessage = `
+  if (alertMessage) {
+    const emailMessage = `
         Your alert${alertName ? ` ${alertName}` : ''} has been triggered.
 
         Layer: ${serverLayerName}
@@ -41,22 +32,31 @@ async function run() {
   
         Alert: ${alertMessage}`;
 
-        console.log(
-          `Alert ${id} - '${alert.alertName}' was triggered on ${maxDate}.`,
-        );
-        // TODO - Send an email using WFP SMTP servers.
-        await sendEmail({
-          from: 'prism-alert@ovio.org',
-          to: email,
-          subject: `PRISM Alert Triggered`,
-          text: emailMessage,
-        });
+    console.log(
+      `Alert ${id} - '${alert.alertName}' was triggered on ${maxDate}.`,
+    );
+    // TODO - Send an email using WFP SMTP servers.
+    await sendEmail({
+      from: 'prism-alert@ovio.org',
+      to: email,
+      subject: `PRISM Alert Triggered`,
+      text: emailMessage,
+    });
 
-        console.log(alertMessage);
-      }
-      // Update lastTriggered (imnactive during testing)
-      await alertRepository.update(alert.id, { lastTriggered: maxDate });
-    }),
+    console.log(alertMessage);
+  }
+  // Update lastTriggered (imnactive during testing)
+  await alertRepository.update(alert.id, { lastTriggered: maxDate });
+}
+async function run() {
+  const connection = await createConnection();
+  const alertRepository = connection.getRepository(Alert);
+
+  const alerts = await alertRepository.find();
+  console.info(`Processing ${alerts.length} alerts.`);
+
+  await Promise.all(
+    alerts.map(async (alert) => processAlert(alert, alertRepository)),
   );
 }
 
