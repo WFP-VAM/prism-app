286645f27c106a2ca4813612fd135fc77791bfd2
Jorge Martinez <jorgemg0327@gmail.com>
2021-05-18 16:42:32 -0500

    Create tests

--- api-flask/app/tests/test_calculate.py
+++ api-flask/app/tests/test_calculate.py
@@ -32,3 +32,22 @@ def test_calculate_stats_with_group_by():
         zones, geotiff, group_by='ADM1_PCODE', geojson_out=False)
     assert len(features) == 4
     assert True
+
+
+def test_calculate_stats_wfs_polygons():
+    """ Test calculate_stats with a group_by argument."""
+
+    zones = '/app/tests/small_admin_boundaries.json'
+    geotiff = '/app/tests/raster_sample.tif'
+    wfs_response = '/app/tests/wfs_response.json'
+
+    features = calculate_stats(
+        zones, geotiff, geojson_out=False, wfs_response=wfs_response)
+    assert len(features) == 5
+
+    features = calculate_stats(
+        zones, geotiff, group_by='ADM1_PCODE', geojson_out=False, wfs_response=wfs_response)
+    assert len(features) == 2
+
+    assert True
+
