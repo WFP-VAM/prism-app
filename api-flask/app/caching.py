053b9c7da12f4c6ebbe2d25cbb4cb06452318e07
Eric Boucher <eric.p.boucher@gmail.com>
2021-05-19 11:49:01 -0700

    Fix intermittent geojson cahcing by the API (#194)

--- api-flask/app/caching.py
+++ api-flask/app/caching.py
@@ -14,11 +14,12 @@ CACHE_DIRECTORY = os.getenv('CACHE_DIRECTORY', '/cache/')
 
 
 @timed
-def cache_file(url, prefix):
+def cache_file(url, prefix, extension='cache'):
     """Locally cache files fetched from a url."""
     cache_filepath = _get_cached_filepath(
         prefix=prefix,
         data=url,
+        extension=extension,
     )
     # If the file exists, return path.
     if os.path.isfile(cache_filepath):
@@ -37,12 +38,13 @@ def cache_file(url, prefix):
 
 @timed
 def cache_geojson(geojson, prefix):
-    """Locally store needed for a request."""
+    """Locally store geojson needed for a request."""
     json_string = json.dumps(geojson)
 
     cache_filepath = _get_cached_filepath(
         prefix=prefix,
         data=json_string,
+        extension='json',
     )
 
     with open(cache_filepath, 'w') as f:
@@ -58,11 +60,12 @@ def get_json_file(cached_filepath):
         return json.load(f)
 
 
-def _get_cached_filepath(prefix, data):
+def _get_cached_filepath(prefix, data, extension='cache'):
     """Return the filepath where a cached response would live for the given inputs."""
-    filename = '{prefix}_{hash_string}.cache'.format(
+    filename = '{prefix}_{hash_string}.{extension}'.format(
         prefix=prefix,
         hash_string=_hash_value(data),
+        extension=extension,
     )
     logger.debug('Cached filepath: ' + os.path.join(CACHE_DIRECTORY, filename))
     return os.path.join(CACHE_DIRECTORY, filename)
