ec218d60413c9f64eaa4ee19ba282c44fc021cb4
Amit W <awadhwa@gmail.com>
2021-09-03 11:40:59 -0700

    initial setup for cambodia

---
+++ src/config/cambodia/index.ts
@@ -0,0 +1,5 @@
+import cambodiaConfig from './prism.json';
+import cambodiaRawLayers from './layers.json';
+import cambodiaRawTables from './tables.json';
+
+export { cambodiaConfig, cambodiaRawLayers, cambodiaRawTables };
