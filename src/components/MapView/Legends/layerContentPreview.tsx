5ed55b5a99fa531d47b3c99cb917b1f402de5139
Eugene Rwagasore <rwagasore@gmail.com>
2021-09-09 17:40:30 +0200

    feat: added support for using one `md` file and ref with id

--- src/components/MapView/Legends/layerContentPreview.tsx
+++ src/components/MapView/Legends/layerContentPreview.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { parse } from 'htmlstring-to-react';
 import marked from 'marked';
 import DOMPurify from 'dompurify';
 import {
@@ -19,20 +20,39 @@ import { LayerDefinitions } from '../../../config/utils';
 const LayerContentPreview = ({ layerId, classes }: PreviewProps) => {
   const [open, setOpen] = useState(false);
   const [content, setContent] = useState('');
+  const contentRef = useRef<HTMLHeadingElement>(null);
   const layer = LayerDefinitions[layerId || 'admin_boundaries'];
   const canDisplayContent = !layer.group || layer.group.main;
   const hasContent = layer.contentPath && layer.contentPath.length > 0;
+
+  const domId = layer.contentPath && layer.contentPath.split('#')[1];
 
   marked.use({ sanitizer: DOMPurify.sanitize });
 
   useEffect(() => {
     const path = `${process.env.PUBLIC_URL}/${layer.contentPath}`;
+
     if (hasContent) {
       fetch(path)
         .then(response => response.text())
         .then(text => setContent(text));
     }
   }, [hasContent, layer.contentPath]);
+
+  const transform = {
+    overrides: {
+      h1: (props: any, textContext: string | undefined) => {
+        const { id } = props;
+        if (contentRef.current && id && id === domId) {
+          contentRef.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
+        }
+        return <h1 ref={contentRef}>{textContext}</h1>;
+      },
+    },
+  };
 
   return (
     <Grid item>
@@ -50,12 +70,7 @@ const LayerContentPreview = ({ layerId, classes }: PreviewProps) => {
       >
         <DialogContent>
           <Typography color="textSecondary">
-            <div
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{
-                __html: marked(content),
-              }}
-            />
+            {parse(marked(content), transform)}
           </Typography>
         </DialogContent>
       </Dialog>
