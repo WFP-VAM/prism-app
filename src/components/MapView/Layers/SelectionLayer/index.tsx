ea97ae1e08f131a788217a426b1cbf7cf6a85436
Ryan <ryan2002.rs@gmail.com>
2021-11-22 19:05:48 +1100

    Fix ci

--- src/components/MapView/Layers/SelectionLayer/index.tsx
+++ src/components/MapView/Layers/SelectionLayer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { GeoJSONLayer } from 'react-mapbox-gl';
 import * as MapboxGL from 'mapbox-gl';
 import {
