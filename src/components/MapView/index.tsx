e85705b8791c9ef81e391fd1d4f42a495e2cdd36
Jorge Martínez Gómez <jorgemg0327@gmail.com>
2021-11-23 20:31:51 +0100

    Style and include multiple admin boundaries as separate layers (#306)
    * Style and include multiple admin boundaries as separate layers
    
    * adding myanmar for testing
    
    * Modify message for empty array
    
    * Update failing snapshots
    
    Co-authored-by: Amit W <awadhwa@gmail.com>

--- src/components/MapView/index.tsx
+++ src/components/MapView/index.tsx
@@ -44,6 +44,7 @@ import { useUrlHistory } from '../../utils/url-utils';
 
 import {
   getBoundaryLayerSingleton,
+  getBoundaryLayers,
   LayerDefinitions,
 } from '../../config/utils';
 
@@ -211,11 +212,13 @@ function MapView({ classes }: MapViewProps) {
 
   useEffect(() => {
     dispatch(loadAvailableDates());
-    dispatch(addLayer(boundaryLayer));
+    const boundaryLayers = getBoundaryLayers();
+
     // we must load boundary layer here for two reasons
     // 1. Stop showing two loading screens on startup - Mapbox renders its children very late, so we can't rely on BoundaryLayer to load internally
     // 2. Prevent situations where a user can toggle a layer like NSO (depends on Boundaries) before Boundaries finish loading.
-    dispatch(loadLayerData({ layer: boundaryLayer }));
+    boundaryLayers.map(l => dispatch(addLayer(l)));
+    boundaryLayers.map(l => dispatch(loadLayerData({ layer: l })));
   }, [boundaryLayer, dispatch]);
 
   // calculate possible dates user can pick from the currently selected layers
