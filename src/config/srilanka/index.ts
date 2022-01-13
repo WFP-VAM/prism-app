16863b31c5037b7cd496564c851f28065be73797
Amit W <awadhwa@gmail.com>
2022-01-12 22:21:50 -0800

    reconfigure sri lanka

---
+++ src/config/srilanka/index.ts
@@ -0,0 +1,5 @@
+import srilankaConfig from './prism.json';
+import srilankaRawLayers from './layers.json';
+import srilankaRawTables from './tables.json';
+
+export { srilankaConfig, srilankaRawLayers, srilankaRawTables };
