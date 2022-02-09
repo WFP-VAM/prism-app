26cc5e0ba0fe1f95f88cd37049bf897b16eb81b3
Eugene Rwagasore <rwagasore@gmail.com>
2022-02-09 16:31:41 +0100

    Re-organized chart state slice into one file

---
+++ src/context/chartDataStateSlice.ts
@@ -0,0 +1,67 @@
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
+import * as Papa from 'papaparse';
+import type { CreateAsyncThunkTypes, RootState } from './store';
+import { TableData } from './tableStateSlice';
+
+type DatasetState = {
+  data?: TableData;
+};
+
+const initialState: DatasetState = {};
+
+export type DatasetParams = {
+  id: string;
+  filepath: string;
+};
+
+export const loadDataset = createAsyncThunk<
+  TableData,
+  DatasetParams,
+  CreateAsyncThunkTypes
+>('datasetState/loadDataset', async (params: DatasetParams) => {
+  const url = process.env.PUBLIC_URL + params.filepath;
+
+  return new Promise<TableData>((resolve, reject) =>
+    Papa.parse(url, {
+      header: true,
+      download: true,
+      complete: results => {
+        const row = results.data.find(item => item.Admin2_Code === params.id);
+
+        return resolve({
+          rows: [...results.data.slice(0, 1), row],
+          columns: Object.keys(row),
+        });
+      },
+      error: error => reject(error),
+    }),
+  );
+});
+
+export const datasetResultStateSlice = createSlice({
+  name: 'DatasetResultSlice',
+  initialState,
+  reducers: {
+    addEwsDataset: (
+      { ...rest },
+      { payload }: PayloadAction<TableData>,
+    ): DatasetState => ({ ...rest, data: payload }),
+  },
+  extraReducers: builder => {
+    builder.addCase(
+      loadDataset.fulfilled,
+      ({ ...rest }, { payload }: PayloadAction<TableData>): DatasetState => ({
+        ...rest,
+        data: payload,
+      }),
+    );
+  },
+});
+
+export const DatasetSelector = (state: RootState): TableData | undefined =>
+  state.datasetState.data;
+
+// Setters
+export const { addEwsDataset } = datasetResultStateSlice.actions;
+
+export default datasetResultStateSlice.reducer;
