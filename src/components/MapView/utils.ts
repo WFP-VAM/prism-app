843b017f18d500c922a87b806ab81bc4ee15b114
Eric Boucher <eric.p.boucher@gmail.com>
2022-03-07 23:25:38 +0100

    Update utils.ts

--- src/components/MapView/utils.ts
+++ src/components/MapView/utils.ts
@@ -21,7 +21,7 @@ import { ExposedPopulationResult } from '../../utils/analysis-utils';
 import { TableData } from '../../context/tableStateSlice';
 
 export function getRoundedData(data: number, decimals: number = 3): string {
-  return data ? data.toFixed(decimals).toLocaleString() : 'No Data';
+  return data ? parseFloat(data.toFixed(decimals)).toString() : 'No Data';
 }
 
 export const getActiveFeatureInfoLayers = (map: Map): WMSLayerProps[] => {
