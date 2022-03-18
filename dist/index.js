aceb5f99ddc1845a300b9906828c1fdb7ea4b5b7
Eugene Rwagasore <rwagasore@gmail.com>
2022-03-18 15:51:34 +0100

    Added custom github action boilerplate

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+(async () => {
+  console.warn('Hello');
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
---
+++ scripts/idpoor-action.js
@@ -0,0 +1,3 @@
+(async () => {
+  console.warn('Hello');
+})();
