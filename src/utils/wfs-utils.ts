7749e380d1042b80c7394fc3ce2049a2065cd404
DanielJDufour <daniel.j.dufour@gmail.com>
2022-03-30 15:49:13 -0400

    added wfs-utils including function fetchWMSLayerAsGeoJSON

---
+++ src/utils/wfs-utils.ts
@@ -0,0 +1,37 @@
+import { FeatureCollection } from 'geojson';
+
+import { WMSLayerProps } from '../config/types';
+
+interface propTypes {
+  lyr: WMSLayerProps;
+  startDate: any;
+  endDate: any;
+}
+
+export function fetchWMSLayerAsGeoJSON(
+  options: propTypes,
+): Promise<FeatureCollection> {
+  const { lyr, startDate, endDate } = options;
+
+  if (lyr.type !== 'wms') {
+    throw Error('unexpected layer type');
+  }
+
+  const wfsServerURL = `${lyr.baseUrl}/wfs`;
+
+  const params = new URLSearchParams();
+  params.set('SERVICE', 'WFS');
+  params.set('request', 'GetFeature');
+  params.set('typeNames', lyr.serverLayerName); // per https://docs.geoserver.org/latest/en/user/services/wfs/reference.html
+  params.set('outputFormat', 'application/json'); // per https://docs.geoserver.org/latest/en/user/services/wfs/outputformats.html
+
+  if (startDate && endDate) {
+    params.set('cql_filter', `timestamp BETWEEN ${startDate} AND ${endDate}`);
+  }
+
+  // may want/need to add additionalQueryParams like srsName and bbox
+
+  const url = `${wfsServerURL}?${params.toString()}`;
+
+  return fetch(url).then(r => r.json());
+}
