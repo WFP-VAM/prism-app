056738857f58d6aa3d335b0c0d4767561510ab2f
tdlinh2712 <tdlinh2712@gmail.com>
2022-04-05 23:34:15 +1000

    add translation for layer

--- src/config/myanmar/index.ts
+++ src/config/myanmar/index.ts
@@ -1,11 +1,14 @@
 import appConfig from './prism.json';
 import rawLayers from './layers.json';
+import myanmarTranslation from './translation.json';
 
 const rawTables = {};
+const translation = { kh: myanmarTranslation };
 
 export default {
   appConfig,
   rawLayers,
   rawTables,
+  translation,
   defaultBoundariesFile: 'mmr_admin_boundaries.json',
 };
