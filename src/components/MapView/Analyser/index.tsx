4e9361db6ddd739dd499ee54e1131b1a78f8556f
tdlinh2712 <tdlinh2712@gmail.com>
2022-04-06 00:19:57 +1000

    add translation for dropdown and analysis tabel

--- src/components/MapView/Analyser/index.tsx
+++ src/components/MapView/Analyser/index.tsx
@@ -432,9 +432,9 @@ function Analyser({ extent, classes }: AnalyserProps) {
                   </div>
 
                   <div className={classes.analyserOptions}>
-                    <Typography variant="body2">Date Range</Typography>
+                    <Typography variant="body2">{t('Date Range')}</Typography>
                     <div className={classes.dateRangePicker}>
-                      <Typography variant="body2">Start</Typography>
+                      <Typography variant="body2">{t('Start')}</Typography>
                       <DatePicker
                         selected={startDate ? new Date(startDate) : null}
                         onChange={date =>
@@ -452,7 +452,7 @@ function Analyser({ extent, classes }: AnalyserProps) {
                       />
                     </div>
                     <div className={classes.dateRangePicker}>
-                      <Typography variant="body2">End</Typography>
+                      <Typography variant="body2">{t('End')}</Typography>
                       <DatePicker
                         selected={endDate ? new Date(endDate) : null}
                         onChange={date =>
