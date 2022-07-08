491119123ac3d6561fa5f02958f4d531bf44989c
Eric Boucher <eric.p.boucher@gmail.com>
2022-07-08 14:51:19 +0200

    reformat using black

--- api-flask/app/caching.py
+++ api-flask/app/caching.py
@@ -14,11 +14,11 @@ from werkzeug.exceptions import InternalServerError
 
 logger = logging.getLogger(__name__)
 
-CACHE_DIRECTORY = os.getenv('CACHE_DIRECTORY', '/cache/')
+CACHE_DIRECTORY = os.getenv("CACHE_DIRECTORY", "/cache/")
 
 
 @timed
-def cache_file(url, prefix, extension='cache'):
+def cache_file(url, prefix, extension="cache"):
     """Locally cache files fetched from a url."""
     cache_filepath = _get_cached_filepath(
         prefix=prefix,
@@ -27,10 +27,10 @@ def cache_file(url, prefix, extension='cache'):
     )
     # If the file exists, return path.
     if os.path.isfile(cache_filepath):
-        logger.info('Returning cached file for {}.'.format(url))
+        logger.info("Returning cached file for {}.".format(url))
 
         # if the file is a geotiff, confirm that we can open it.
-        if extension == 'tif':
+        if extension == "tif":
             try:
                 rasterio.open(cache_filepath)
                 return cache_filepath
@@ -46,14 +46,14 @@ def cache_file(url, prefix, extension='cache'):
         response.raise_for_status()
     except requests.HTTPError as e:
         logger.error(e)
-        raise InternalServerError('The file you requested is not available - {url}'.format(
-            url=url)
+        raise InternalServerError(
+            "The file you requested is not available - {url}".format(url=url)
         )
 
-    with open(cache_filepath, 'wb') as f:
+    with open(cache_filepath, "wb") as f:
         f.write(response.content)
 
-    logger.info('Caching file for {}.'.format(url))
+    logger.info("Caching file for {}.".format(url))
     return cache_filepath
 
 
@@ -65,33 +65,33 @@ def cache_geojson(geojson, prefix):
     cache_filepath = _get_cached_filepath(
         prefix=prefix,
         data=json_string,
-        extension='json',
+        extension="json",
     )
 
-    with open(cache_filepath, 'w') as f:
+    with open(cache_filepath, "w") as f:
         f.write(json_string)
 
-    logger.info('Caching geojson in file.')
+    logger.info("Caching geojson in file.")
     return cache_filepath
 
 
 def get_json_file(cached_filepath):
     """Return geojson object as python dictionary."""
-    with open(cached_filepath, 'rb') as f:
+    with open(cached_filepath, "rb") as f:
         return json.load(f)
 
 
-def _get_cached_filepath(prefix, data, extension='cache'):
+def _get_cached_filepath(prefix, data, extension="cache"):
     """Return the filepath where a cached response would live for the given inputs."""
-    filename = '{prefix}_{hash_string}.{extension}'.format(
+    filename = "{prefix}_{hash_string}.{extension}".format(
         prefix=prefix,
         hash_string=_hash_value(data),
         extension=extension,
     )
-    logger.debug('Cached filepath: ' + os.path.join(CACHE_DIRECTORY, filename))
+    logger.debug("Cached filepath: " + os.path.join(CACHE_DIRECTORY, filename))
     return os.path.join(CACHE_DIRECTORY, filename)
 
 
 def _hash_value(value):
     """Hash value to help identify what cached file to use."""
-    return hashlib.md5(value.encode('utf-8')).hexdigest()[:9]
+    return hashlib.md5(value.encode("utf-8")).hexdigest()[:9]
