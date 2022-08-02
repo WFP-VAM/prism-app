8687c66e8eb1929e92259a3b1e68e420a5c1b1e7
Denys Pysarenko <xxxd0nixxx1993@gmail.com>
2022-08-02 15:27:12 +0300

    Remove unused countries from build

---
+++ config-overrides.js
@@ -0,0 +1,30 @@
+const {
+  override,
+  disableChunk,
+  useEslintRc,
+  addWebpackPlugin,
+} = require('customize-cra');
+const path = require('path');
+const RemovePlugin = require('remove-files-webpack-plugin');
+
+const country = process.env.REACT_APP_COUNTRY;
+
+module.exports = override(
+  useEslintRc(path.resolve(__dirname, '.eslintrc')),
+  disableChunk(),
+  !!country &&
+    addWebpackPlugin(
+      new RemovePlugin({
+        after: {
+          root: './build',
+          test: [
+            {
+              folder: './data',
+              method: absPath => !new RegExp(country, 'm').test(absPath),
+              recursive: true,
+            },
+          ],
+        },
+      }),
+    ),
+);
