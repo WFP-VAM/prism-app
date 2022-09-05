9ae7d0110ba8a2194cbcc9fb447721e70dc22d7c
DanielJDufour <daniel.j.dufour@gmail.com>
2022-09-05 13:38:34 -0400

    added scale-image

---
+++ packages/ogc/src/common/scale-image.ts
@@ -0,0 +1,35 @@
+import checkExtent from "./check-extent";
+import type { BBOX } from "../types";
+
+export default function scaleImage(extent: BBOX,
+  {
+    check_extent = true,
+    resolution = 256,
+    max_pixels = 5096
+  }: {
+    check_extent?: boolean;
+    resolution?: number;
+    max_pixels?: number;
+  } = {
+    check_extent: true,
+    resolution: 256,
+    max_pixels: 5096
+  }
+) {
+  if (check_extent) checkExtent(extent);
+
+  const [minX, minY, maxX, maxY] = extent;
+
+  // Get our image width & height at either the desired resolution or a down-sampled resolution if the resulting
+  // dimensions would exceed our `maxPixels` in height or width
+  const xRange = maxX - minX;
+  const yRange = maxY - minY;
+
+  const maxDim = Math.min(max_pixels, xRange * resolution, yRange * resolution);
+  const scale = maxDim / Math.max(xRange, yRange);
+
+  const width = Math.ceil(xRange * scale);
+  const height = Math.ceil(yRange * scale);
+
+  return { width, height };
+}
