f61264acf0e014faa1e06ea96e51c1761f138256
Eric Boucher <eric.p.boucher@gmail.com>
2022-09-17 14:44:39 +0200

    Update index.tsx

--- src/components/MapView/Layers/AnalysisLayer/index.tsx
+++ src/components/MapView/Layers/AnalysisLayer/index.tsx
@@ -18,7 +18,6 @@ import {
 import { getRoundedData } from '../../../../utils/data-utils';
 import { useSafeTranslation } from '../../../../i18n';
 import { LayerDefinitions } from '../../../../config/utils';
-import { firstBoundaryOnView } from '../../../../utils/map-utils';
 
 function AnalysisLayer({ before }: { before?: string }) {
   // TODO maybe in the future we can try add this to LayerType so we don't need exclusive code in Legends and MapView to make this display correctly
