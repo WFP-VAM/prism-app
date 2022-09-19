8a96289664be65fa15a56896cfc4aadab2d8b4e0
Amit W <awadhwa@gmail.com>
2022-09-18 22:22:49 -0700

    initial configuration for prism south sudan

---
+++ src/config/southsudan/index.ts
@@ -0,0 +1,11 @@
+import appConfig from './prism.json';
+import rawLayers from './layers.json';
+
+const rawTables = {};
+
+export default {
+  appConfig,
+  rawLayers,
+  rawTables,
+  defaultBoundariesFile: 'wca_admbnda_adm2_ocha.json',
+};
