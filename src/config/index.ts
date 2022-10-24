1071382c42e46468dfe27a2edd2fd0a6dccb07a8
Amit Wadhwa <awadhwa@gmail.com>
2022-10-24 09:50:16 -0700

    update config with new color ramps and layer grouping; COUNTRY=namibia (#611)
    * update nambia config with new color ramps and layer grouping
    
    * additional layer config changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -32,7 +32,7 @@ import mozambique from './mozambique';
 
 import myanmar from './myanmar';
 
-import { namibiaConfig, namibiaRawLayers, namibiaRawTables } from './namibia';
+import namibia from './namibia';
 
 import rbd from './rbd';
 
@@ -77,12 +77,7 @@ const configMap = {
   },
   mozambique,
   myanmar,
-  namibia: {
-    appConfig: namibiaConfig,
-    rawLayers: namibiaRawLayers,
-    rawTables: namibiaRawTables,
-    defaultBoundariesFile: 'nam_admin2.json',
-  },
+  namibia,
   rbd,
   sierraleone,
   southsudan,
