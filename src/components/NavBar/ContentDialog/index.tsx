0e60fbc6231fcb114923107b0e9f82c2999feadd
Jorge Martinez <jorgemg0327@gmail.com>
2022-10-26 09:07:06 +0200

    Support markdown for about link

---
+++ src/components/NavBar/ContentDialog/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import parse from 'html-react-parser';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
+import { Dialog, DialogContent, Typography } from '@material-ui/core';
+
+type ContentDialogProps = {
+  content?: string;
+  setContent: (value?: string) => void;
+};
+
+export const loadLayerContent = async (
+  path: string,
+  setContent: (value?: string) => void,
+) => {
+  const resp = await fetch(path);
+  const respText = await resp.text();
+
+  setContent(respText);
+};
+
+const ContentDialog = ({ content, setContent }: ContentDialogProps) => {
+  const [open, setOpen] = useState(false);
+
+  marked.use({ sanitizer: DOMPurify.sanitize });
+
+  useEffect(() => {
+    if (!content) {
+      setOpen(false);
+      return;
+    }
+    setOpen(true);
+  }, [content]);
+
+  return (
+    <Dialog
+      maxWidth="md"
+      open={open}
+      keepMounted
+      onClose={() => setContent(undefined)}
+      aria-labelledby="dialog-preview"
+    >
+      <DialogContent>
+        <Typography color="textSecondary">
+          {content && parse(marked(content))}
+        </Typography>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ContentDialog;
