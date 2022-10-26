4cba840b32fbc8bd53f4962df6f8307b2d7784d2
Eric Boucher <eric.p.boucher@gmail.com>
2022-10-26 11:59:02 +0200

    Update utils.py

--- api/app/utils.py
+++ api/app/utils.py
@@ -5,12 +5,11 @@ from fastapi import HTTPException
 
 
 def forward_http_error(resp: requests.Response, excluded_codes: list[int]) -> None:
-    if not resp.status_code > 399: return
+    if not resp.status_code > 399:
+        return
 
     if excluded_codes and resp.status_code in excluded_codes:
-        raise HTTPException(
-            status_code=500, detail="An internal error occurred."
-        )
+        raise HTTPException(status_code=500, detail="An internal error occurred.")
     else:
         obj = resp.json()
         detail = obj.get("detail", "Unknown error")
