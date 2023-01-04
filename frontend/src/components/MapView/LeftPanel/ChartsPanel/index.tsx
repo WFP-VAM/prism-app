57b2325b825724f0334792e18942dc72623261ce
sife <felixv@sicara.com>
2023-01-04 11:32:18 -0500

    Store the admin properties

--- frontend/src/components/MapView/LeftPanel/ChartsPanel/index.tsx
+++ frontend/src/components/MapView/LeftPanel/ChartsPanel/index.tsx
@@ -5,6 +5,7 @@ import {
   MenuItem,
   TextField,
 } from '@material-ui/core';
+import { GeoJsonProperties } from 'geojson';
 import React, { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { BoundaryLayerProps } from '../../../../config/types';
@@ -15,6 +16,15 @@ import { useSafeTranslation } from '../../../../i18n';
 import { getCategories } from '../../Layers/BoundaryDropdown';
 
 const boundaryLayer = getBoundaryLayerSingleton();
+
+function getProperties(
+  id: string,
+  layerData: LayerData<BoundaryLayerProps>['data'],
+) {
+  return layerData.features.filter(
+    elem => elem.properties && elem.properties[boundaryLayer.adminCode] === id,
+  )[0].properties;
+}
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -50,7 +60,8 @@ function ChartsPanel() {
   const classes = useStyles();
   const [admin1Title, setAdmin1Title] = useState('');
   const [admin2Title, setAdmin2Title] = useState('');
-  const { i18n: i18nLocale } = useSafeTranslation();
+  const [adminProperties, setAdminProperties] = useState<GeoJsonProperties>();
+  const { t, i18n: i18nLocale } = useSafeTranslation();
   const boundaryLayerData = useSelector(layerDataSelector(boundaryLayer.id)) as
     | LayerData<BoundaryLayerProps>
     | undefined;
@@ -60,15 +71,39 @@ function ChartsPanel() {
     [data, i18nLocale],
   );
 
+  const onChangeAdmin1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // The external chart key for admin 1 is stored in all its children regions
+    // here we get the first child properties
+    const admin2Id = categories.filter(
+      elem => elem.title === event.target.value,
+    )[0].children[0].value;
+
+    if (data) {
+      setAdminProperties(getProperties(admin2Id, data));
+    }
+    setAdmin1Title(event.target.value);
+    setAdmin2Title('');
+  };
+
+  const onChangeAdmin2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const admin2Id = categories
+      .filter(elem => elem.title === admin1Title)[0]
+      .children.filter(elem => elem.label === event.target.value)[0].value;
+    if (data) {
+      setAdminProperties(getProperties(admin2Id, data));
+    }
+    setAdmin2Title(event.target.value);
+  };
+
   return (
     <Box className={classes.chartsPanelParams}>
       <TextField
         classes={{ root: classes.selectRoot }}
         id="outlined-admin-1"
         select
-        label="Select Admin 1"
+        label={t('Select Admin 1')}
         value={admin1Title}
-        onChange={e => setAdmin1Title(e.target.value)}
+        onChange={onChangeAdmin1}
         variant="outlined"
       >
         {categories.map(option => (
@@ -82,12 +117,9 @@ function ChartsPanel() {
           classes={{ root: classes.selectRoot }}
           id="outlined-admin-2"
           select
-          label="Select Admin 2"
+          label={t('Select Admin 2')}
           value={admin2Title}
-          onChange={e => {
-            console.log(e);
-            setAdmin2Title(e.target.value);
-          }}
+          onChange={onChangeAdmin2}
           variant="outlined"
         >
           {categories
