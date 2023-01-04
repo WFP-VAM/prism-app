1074d990541ce59b4b0f9470c7aca8d5bbc3179d
sife <felixv@sicara.com>
2023-01-04 09:56:42 -0500

    Add dropdown for admin1

--- frontend/src/components/MapView/LeftPanel/ChartsPanel/index.tsx
+++ frontend/src/components/MapView/LeftPanel/ChartsPanel/index.tsx
@@ -1,8 +1,83 @@
-import { Box } from '@material-ui/core';
-import React from 'react';
+import {
+  Box,
+  createStyles,
+  makeStyles,
+  MenuItem,
+  TextField,
+} from '@material-ui/core';
+import React, { useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { BoundaryLayerProps } from '../../../../config/types';
+import { getBoundaryLayerSingleton } from '../../../../config/utils';
+import { LayerData } from '../../../../context/layers/layer-data';
+import { layerDataSelector } from '../../../../context/mapStateSlice/selectors';
+import { useSafeTranslation } from '../../../../i18n';
+import { getCategories } from '../../Layers/BoundaryDropdown';
+
+const boundaryLayer = getBoundaryLayerSingleton();
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    selectRoot: {
+      color: 'black',
+      minWidth: '300px',
+      maxWidth: '350px',
+      width: 'auto',
+      '& label': {
+        color: '#333333',
+      },
+      '& .MuiInputBase-root': {
+        color: 'black',
+        '&:hover fieldset': {
+          borderColor: '#333333',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: '#333333',
+        },
+      },
+    },
+    chartsPanelParams: {
+      marginTop: 30,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+  }),
+);
 
 function ChartsPanel() {
-  return <Box>Charts</Box>;
+  const classes = useStyles();
+  const [admin1Title, setAdmin1Title] = useState('');
+  const { i18n: i18nLocale } = useSafeTranslation();
+  const boundaryLayerData = useSelector(layerDataSelector(boundaryLayer.id)) as
+    | LayerData<BoundaryLayerProps>
+    | undefined;
+  const { data } = boundaryLayerData || {};
+  const categories = useMemo(
+    () => (data ? getCategories(data, boundaryLayer, '', i18nLocale) : []),
+    [data, i18nLocale],
+  );
+
+  return (
+    <Box className={classes.chartsPanelParams}>
+      <TextField
+        classes={{ root: classes.selectRoot }}
+        id="outlined-admin-1"
+        select
+        label="Select Admin 1"
+        value={admin1Title}
+        onChange={e => setAdmin1Title(e.target.value)}
+        variant="outlined"
+      >
+        {categories.map(option => (
+          <MenuItem key={option.title} value={option.title}>
+            {option.title}
+          </MenuItem>
+        ))}
+      </TextField>
+    </Box>
+  );
 }
 
 export default ChartsPanel;
