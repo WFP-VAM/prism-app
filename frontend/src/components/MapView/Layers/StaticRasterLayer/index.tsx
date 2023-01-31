eafe8a2c2ed489628c956d909c10d0ee21fee9f2
Jorge Martinez <jorgemg0327@gmail.com>
2023-01-31 10:34:35 +0100

    set xyz url parameters from layers configuration file

--- frontend/src/components/MapView/Layers/StaticRasterLayer/index.tsx
+++ frontend/src/components/MapView/Layers/StaticRasterLayer/index.tsx
@@ -3,6 +3,7 @@ import moment from 'moment';
 import { Layer, Source } from 'react-mapbox-gl';
 import { StaticRasterLayerProps } from '../../../../config/types';
 import { useDefaultDate } from '../../../../utils/useDefaultDate';
+import { DEFAULT_DATE_FORMAT_SNAKE_CASE } from '../../../../utils/name-utils';
 
 function StaticRasterLayer({
   layer: { id, baseUrl, opacity, minZoom, maxZoom, dates },
@@ -10,7 +11,10 @@ function StaticRasterLayer({
 }: LayersProps) {
   const selectedDate = useDefaultDate(id);
   const url = dates
-    ? `${baseUrl}/${moment(selectedDate).format('YYYY_MM_DD')}/{z}/{x}/{y}.png`
+    ? baseUrl.replace(
+        `{${DEFAULT_DATE_FORMAT_SNAKE_CASE}}`,
+        moment(selectedDate).format(DEFAULT_DATE_FORMAT_SNAKE_CASE),
+      )
     : baseUrl;
   return (
     <>
--- frontend/src/components/MapView/index.tsx
+++ frontend/src/components/MapView/index.tsx
@@ -201,7 +201,7 @@ function MapView({ classes }: MapViewProps) {
   );
   const selectedLayersWithDateSupport = selectedLayers
     .filter((layer): layer is DateCompatibleLayer => {
-      if (layer.type === 'admin_level_data') {
+      if (layer.type === 'admin_level_data' || layer.type === 'static_raster') {
         return Boolean(layer.dates);
       }
       if (layer.type === 'wms') {
