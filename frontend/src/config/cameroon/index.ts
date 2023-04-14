2e740bf344431f76fee4c47aba1aa4b915d0a1ba
Amit W <awadhwa@gmail.com>
2023-04-13 19:19:26 -0700

    initial configuration for cameroon (on new UX)

---
+++ frontend/src/config/cameroon/index.ts
@@ -0,0 +1,13 @@
+import appConfig from './prism.json';
+import rawLayers from './layers.json';
+
+const rawTables = {};
+const translation = {};
+
+export default {
+  appConfig,
+  rawLayers,
+  rawTables,
+  translation,
+  defaultBoundariesFile: 'cmr_admbnda_adm2_wfp_ocha.json',
+};
