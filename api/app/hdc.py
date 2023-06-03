cb384c7178cdff836c0836a19a1780882ac4a9c2
Ridwan Mulyadi <ridwan.mulyadi@live.com>
2023-06-03 11:57:33 +0700

    python formatting

--- api/app/hdc.py
+++ api/app/hdc.py
@@ -12,12 +12,7 @@ if hdc_token == "":
 
 
 def get_hdc_stats(
-    level: str,
-    id_code: str,
-    coverage: str,
-    vam: str,
-    start: str,
-    end: str
+    level: str, id_code: str, coverage: str, vam: str, start: str, end: str
 ):
     """Get statistical charts data"""
     url = "https://api.wfp.org/hdc/1.0.0/stats/admin"
@@ -27,12 +22,12 @@ def get_hdc_stats(
         "coverage": coverage,
         "vam": vam,
         "start": start,
-        "end": end
+        "end": end,
     }
-    response = requests.get(url, params=params, headers={
-        "Authorization": f"Bearer {hdc_token}",
-        "Accept": "application/json"
-    })
+    response = requests.get(
+        url,
+        params=params,
+        headers={"Authorization": f"Bearer {hdc_token}", "Accept": "application/json"},
+    )
     response.raise_for_status()
     return response.json()
-
