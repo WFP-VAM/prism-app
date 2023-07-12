bf6002ab37dd92ef61f9da472427d8d03a52b616
Julien Parmentier <julien.parmentier@pathtech.fr>
2023-07-12 18:01:16 +0200

    feat: add playwright dependency in req and update function

--- api/app/playwrigh-renderer-poc/main.py
+++ api/app/playwrigh-renderer-poc/main.py
@@ -1,21 +1,23 @@
 from playwright.sync_api import sync_playwright
 
 
-# TODO: Needed layerIds as parameter
-def main():
+def playwright_download_report(url, language):
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
         page.set_default_timeout(0)
-        page.goto("http://localhost:3000/?hazardLayerIds=flood_extent&date=2023-07-07")
+        page.goto(url)
+
         page.query_selector('div[id="Flood"]').click()
         page.query_selector('p:has-text("Flood Monitoring")').click()
         page.query_selector('button[title="Exposure Analysis"]').click()
         create_report_selector = 'p:has-text("Create Report")'
         page.wait_for_selector(create_report_selector, state="visible")
+
+        page.query_selector('p:has-text("' + language + '")').click()
         page.query_selector(create_report_selector).click()
 
-        download_report_selector = 'a:has-text("Download")'
+        download_report_selector = "a[download]"
         page.wait_for_selector(download_report_selector)
 
         with page.expect_download() as download_info:
@@ -24,6 +26,12 @@ def main():
         download = download_info.value
         download.save_as("./report.pdf")
         browser.close()
+
+
+def main():
+    playwright_download_report(
+        "http://localhost:3000/?hazardLayerIds=flood_extent&date=2023-07-07", "en"
+    )
 
 
 main()
