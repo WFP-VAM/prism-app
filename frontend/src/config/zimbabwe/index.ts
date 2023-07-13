2811fd50362fe7d6e2ef40c527da4a6bf0a70e8e
Amit W <awadhwa@gmail.com>
2023-07-13 09:01:12 -0700

    Add admin3 boundary; fix issue admin code issue

--- frontend/src/config/zimbabwe/index.ts
+++ frontend/src/config/zimbabwe/index.ts
@@ -2,14 +2,12 @@ import appConfig from './prism.json';
 import rawLayers from './layers.json';
 
 const rawTables = {};
-const rawReports = {};
 const translation = {};
 
 export default {
   appConfig,
   rawLayers,
   rawTables,
-  rawReports,
   translation,
-  defaultBoundariesFile: 'zim_admin2_boundaries_v2.json',
+  defaultBoundariesFile: 'zwe_polbnda_adm3_250k_cso_dv_merged.json',
 };
