8bf530acb58c3da844ba804248a047c41dc0fee4
Eric Boucher <eric.p.boucher@gmail.com>
2023-07-20 17:23:17 +0200

    Update index.ts

--- frontend/src/config/zimbabwe/index.ts
+++ frontend/src/config/zimbabwe/index.ts
@@ -2,12 +2,14 @@ import appConfig from './prism.json';
 import rawLayers from './layers.json';
 
 const rawTables = {};
+const rawReports = {};
 const translation = {};
 
 export default {
   appConfig,
   rawLayers,
   rawTables,
+  rawReports,
   translation,
   defaultBoundariesFile: 'zwe_polbnda_adm3_250k_cso_dv_merged.json',
 };
