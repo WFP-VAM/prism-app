fbe697e7841d47f11d67263637caef61f87738ca
Julien Parmentier <julien.parmentier@pathtech.fr>
2023-07-24 12:44:21 +0200

    fix: switch back to working chromium and refactor with methods

--- api/app/playwright_renderer.py
+++ api/app/playwright_renderer.py
@@ -1,68 +1,57 @@
-# import asyncio
-import asyncio
 import uuid
-from typing import Optional
+from typing import Final, Optional
 from urllib.parse import parse_qs, urlparse
 
 from playwright.async_api import async_playwright
 
+# Html selectors
+LAYER_ACCORDION_SELECTOR: Final[
+    str
+] = 'div[class="MuiAccordionSummary-root"], div[aria-expanded="false"]'
+DOWNLOAD_BUTTON_SELECTOR: Final[str] = "a[download]"
+CREATE_REPORT_BUTTON_SELECTOR: Final[str] = 'button[id="create-report"]'
+
+# Timeouts
+PAGE_TIMEOUT: Final[int] = 25000
+PAGE_LANGUAGE_CHANGE_TIMEOUT: Final[int] = 5000
+
 
 async def playwright_download_report(url: str, language: Optional[str]) -> str:
     async with async_playwright() as p:
-        browser = await p.firefox.launch()
+        browser = await p.chromium.launch()
         page = await browser.new_page()
 
-        page.set_default_timeout(25000)
+        page.set_default_timeout(PAGE_TIMEOUT)
         await page.goto(url)
 
-        parsed_url = urlparse(url)
-        query_params = parse_qs(parsed_url.query)
-        layerIdParam = query_params.get("hazardLayerIds", [""])[0]
+        layerIdParam = extract_layerId_query_param(url)
 
-        layer_chip_selector = (
-            'div[class="MuiAccordionSummary-root"], div[aria-expanded="false"]'
-        )
+        # Wait for page to be loaded
+        await page.wait_for_selector(LAYER_ACCORDION_SELECTOR, state="visible")
 
-        await page.wait_for_selector(layer_chip_selector, state="visible")
+        # Toggle level one dropdowns
+        await toggle_every_visible_dropdown(page)
 
-        dropdown_level_one_selectors = await page.query_selector_all(
-            layer_chip_selector
-        )
-        for dropdown in dropdown_level_one_selectors:
-            await dropdown.click()
+        # Toggle level two dropdowns
+        await toggle_every_visible_dropdown(page)
 
-        dropdown_level_two_selectors = await page.query_selector_all(
-            layer_chip_selector
-        )
+        # Click on exposure analysis toggle button
+        await click_target_exposure_analysis(page, layerIdParam)
 
-        for dropdown in dropdown_level_two_selectors:
-            await dropdown.click()
+        # Wait for page to be loaded on exposure analysis
+        await page.wait_for_selector(CREATE_REPORT_BUTTON_SELECTOR, state="visible")
 
-        selected_exposure_analysis_button = await page.query_selector(
-            'button[id="' + layerIdParam + '"]'
-        )
-        await selected_exposure_analysis_button.click()
+        # Change language if not english
+        await change_language_if_not_default(language, page)
 
-        create_report_selector_query = 'button[id="create-report"]'
-        await page.wait_for_selector(create_report_selector_query, state="visible")
+        await click_create_report_button(page)
 
-        if language and language != "en":
-            language_selector = await page.query_selector(
-                'p:has-text("' + language + '")'
-            )
-            await language_selector.click()
-            await page.wait_for_timeout(5000)
-
-        # await page.pdf(path="page.pdf")
-        create_report_selector = await page.query_selector(create_report_selector_query)
-        await create_report_selector.click()
-
-        download_report_selector_query = "a[download]"
-        await page.wait_for_selector(download_report_selector_query)
+        # Wait for report to be created by pdf-renderer
+        await page.wait_for_selector(DOWNLOAD_BUTTON_SELECTOR)
 
         async with page.expect_download() as download_info:
             download_report_selector = await page.query_selector(
-                download_report_selector_query
+                DOWNLOAD_BUTTON_SELECTOR
             )
             await download_report_selector.click()
 
@@ -83,10 +72,35 @@ async def playwright_download_report(url: str, language: Optional[str]) -> str:
         return str(report_file_path)
 
 
-asyncio.run(
-    playwright_download_report(
-        "http://localhost:3000/?hazardLayerIds=flood_extent&date=2023-07-21", "kh"
-    )
-)
+async def click_create_report_button(page) -> None:
+    create_report_selector = await page.query_selector(CREATE_REPORT_BUTTON_SELECTOR)
+    await create_report_selector.click()
 
-# http://host.docker.internal:3000/?hazardLayerIds=flood_extent&date=2023-07-07
+
+async def change_language_if_not_default(language, page) -> None:
+    if language and language != "en":
+        language_selector = await page.query_selector('p:has-text("' + language + '")')
+        await language_selector.click()
+        await page.wait_for_timeout(PAGE_LANGUAGE_CHANGE_TIMEOUT)
+
+
+async def click_target_exposure_analysis(page, layerIdParam) -> None:
+    selected_exposure_analysis_button = await page.query_selector(
+        'button[id="' + layerIdParam + '"]'
+    )
+    await selected_exposure_analysis_button.click()
+
+
+def extract_layerId_query_param(url) -> str:
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    layerIdParam = query_params.get("hazardLayerIds", [""])[0]
+    return layerIdParam
+
+
+async def toggle_every_visible_dropdown(page) -> None:
+    dropdown_level_one_selectors = await page.query_selector_all(
+        LAYER_ACCORDION_SELECTOR
+    )
+    for dropdown in dropdown_level_one_selectors:
+        await dropdown.click()
