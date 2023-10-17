5ff0d70304730007d30956a5f8c9e36bb6fc7d4b
Ubuntu <ubuntu@ip-172-31-38-62.us-east-2.compute.internal>
2023-10-17 23:11:31 +0000

    Updating to use Secrets

---
+++ api/set_envs.sh
@@ -0,0 +1,13 @@
+# STAC Credentials
+export STAC_AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value     --secret-id STAC_Credentials | jq .SecretString | jq fromjson | jq .AWS_ACCESS_KEY_ID)
+export STAC_AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value     --secret-id STAC_Credentials | jq .SecretString | jq fromjson | jq .AWS_SECRET_ACCESS_KEY)
+
+# Kobo Credentials
+export KOBO_USERNAME=$(aws secretsmanager get-secret-value     --secret-id KOBO_AUTHENTICATION | jq .SecretString | jq fromjson | jq .KOBO_USERNAME)
+export KOBO_PASSWORD=$(aws secretsmanager get-secret-value     --secret-id KOBO_AUTHENTICATION | jq .SecretString | jq fromjson | jq .KOBO_PASSWORD)
+
+# PRISM Alerts
+export PRISM_ALERTS_DATABASE_URL=$(aws secretsmanager get-secret-value     --secret-id PRISM_ALERTS_DATABASE_URL | jq .SecretString | jq fromjson | jq .PRISM_ALERTS_DATABASE_URL)
+
+# HDC Token
+export HDC_TOKEN=$(aws secretsmanager get-secret-value     --secret-id HDC_TOKEN | jq .SecretString | jq fromjson | jq .HDC_TOKEN)
