ee2750372a764d5d37f846ad5939f8d4d5cb8a80
Ubuntu <ubuntu@prism-api.ovio.org>
2023-10-23 18:06:42 +0000

    Fixing raw JSON output

--- api/set_envs.sh
+++ api/set_envs.sh
@@ -1,24 +1,24 @@
 # STAC Credentials
-export STAC_AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value     --secret-id STAC_Credentials | jq .SecretString | jq fromjson | jq .AWS_ACCESS_KEY_ID)
-export STAC_AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value     --secret-id STAC_Credentials | jq .SecretString | jq fromjson | jq .AWS_SECRET_ACCESS_KEY)
+export STAC_AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value     --secret-id STAC_Credentials | jq .SecretString | jq fromjson | jq -r .AWS_ACCESS_KEY_ID)
+export STAC_AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value     --secret-id STAC_Credentials | jq .SecretString | jq fromjson | jq -r .AWS_SECRET_ACCESS_KEY)
 
 # Kobo Credentials
-export KOBO_USERNAME=$(aws secretsmanager get-secret-value     --secret-id KOBO_AUTHENTICATION | jq .SecretString | jq fromjson | jq .KOBO_USERNAME)
-export KOBO_PASSWORD=$(aws secretsmanager get-secret-value     --secret-id KOBO_AUTHENTICATION | jq .SecretString | jq fromjson | jq .KOBO_PASSWORD)
+export KOBO_USERNAME=$(aws secretsmanager get-secret-value     --secret-id KOBO_AUTHENTICATION | jq .SecretString | jq fromjson | jq -r .KOBO_USERNAME)
+export KOBO_PASSWORD=$(aws secretsmanager get-secret-value     --secret-id KOBO_AUTHENTICATION | jq .SecretString | jq fromjson | jq -r .KOBO_PASSWORD)
 
 # PRISM Alerts
-export PRISM_ALERTS_DATABASE_URL=$(aws secretsmanager get-secret-value     --secret-id PRISM_ALERTS_DATABASE_URL | jq .SecretString | jq fromjson | jq .PRISM_ALERTS_DATABASE_URL)
+export PRISM_ALERTS_DATABASE_URL=$(aws secretsmanager get-secret-value     --secret-id PRISM_ALERTS_DATABASE_URL | jq .SecretString | jq fromjson | jq -r .PRISM_ALERTS_DATABASE_URL)
 
 # HDC Token
-export HDC_TOKEN=$(aws secretsmanager get-secret-value     --secret-id HDC_TOKEN | jq .SecretString | jq fromjson | jq .HDC_TOKEN)
+export HDC_TOKEN=$(aws secretsmanager get-secret-value     --secret-id HDC_TOKEN | jq .SecretString | jq fromjson | jq -r .HDC_TOKEN)
 
 # Alerting Email
-export ALERTING_EMAIL_USER=$(aws secretsmanager get-secret-value     --secret-id ALERTING_EMAIL | jq .SecretString | jq fromjson | jq .ALERTING_EMAIL_USER)
-export ALERTING_EMAIL_PASSWORD=$(aws secretsmanager get-secret-value     --secret-id ALERTING_EMAIL | jq .SecretString | jq fromjson | jq .ALERTING_EMAIL_PASSWORD)
+export ALERTING_EMAIL_USER=$(aws secretsmanager get-secret-value     --secret-id ALERTING_EMAIL | jq .SecretString | jq fromjson | jq -r .ALERTING_EMAIL_USER)
+export ALERTING_EMAIL_PASSWORD=$(aws secretsmanager get-secret-value     --secret-id ALERTING_EMAIL | jq .SecretString | jq fromjson | jq -r .ALERTING_EMAIL_PASSWORD)
 
 # ACLED Credentials
-export ACLED_API_KEY=$(aws secretsmanager get-secret-value     --secret-id ACLED_CREDENTIALS | jq .SecretString | jq fromjson | jq .ACLED_API_KEY)
-export ACLED_API_EMAIL=$(aws secretsmanager get-secret-value     --secret-id ACLED_CREDENTIALS | jq .SecretString | jq fromjson | jq .ACLED_API_EMAIL)
+export ACLED_API_KEY=$(aws secretsmanager get-secret-value     --secret-id ACLED_CREDENTIALS | jq .SecretString | jq fromjson | jq -r .ACLED_API_KEY)
+export ACLED_API_EMAIL=$(aws secretsmanager get-secret-value     --secret-id ACLED_CREDENTIALS | jq .SecretString | jq fromjson | jq -r .ACLED_API_EMAIL)
 
 export HOSTNAME=prism-api.ovio.org
 echo $HOSTNAME
