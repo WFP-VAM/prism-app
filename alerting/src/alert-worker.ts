54598c75884b601906b77de3718fee8a81fb76e1
Eric Boucher <eric.p.boucher@gmail.com>
2023-11-02 23:59:51 +0100

    Use bluebird and add better error handling

--- alerting/src/alert-worker.ts
+++ alerting/src/alert-worker.ts
@@ -1,4 +1,5 @@
 import { isNaN } from 'lodash';
+import Bluebird from 'bluebird';
 import nodeFetch from 'node-fetch';
 import { createConnection, Repository } from 'typeorm';
 import { ANALYSIS_API_URL } from './constants';
@@ -6,6 +7,9 @@ import { Alert } from './entities/alerts.entity';
 import { calculateBoundsForAlert } from './utils/analysis-utils';
 import { sendEmail } from './utils/email';
 import { fetchCoverageLayerDays, formatUrl, WMS } from 'prism-common';
+
+// @ts-ignore
+global.fetch = nodeFetch;
 
 async function processAlert(alert: Alert, alertRepository: Repository<Alert>) {
   const {
@@ -24,15 +28,26 @@ async function processAlert(alert: Alert, alertRepository: Repository<Alert>) {
     prismUrl,
     active,
   } = alert;
-  const availableDates =
-    type === 'wms'
-      ? await new WMS(`${baseUrl}/wms`).getLayerDays()
-      : await fetchCoverageLayerDays(baseUrl, { fetch: nodeFetch });
-  const layerAvailableDates = availableDates[serverLayerName];
+
+  let availableDates;
+  let layerAvailableDates = [];
+  try {
+    availableDates = type === 'wms'
+      ? await new WMS(`${baseUrl}/wms`.replace(/([^:]\/)\/+/g, "$1")).getLayerDays()
+      : await fetchCoverageLayerDays(baseUrl);
+    layerAvailableDates = availableDates[serverLayerName];
+  } catch (error) {
+    console.warn(`Failed to fetch available dates for ${baseUrl} ${serverLayerName}: ${(error as Error).message}`);
+  }
+
+  if (!layerAvailableDates) {
+    console.warn(`No dates available for ${baseUrl} ${serverLayerName}.`);
+    return;
+  }
+
   const maxDate = new Date(Math.max(...(layerAvailableDates || [])));
 
   if (
-    !active ||
     isNaN(maxDate.getTime()) ||
     (lastTriggered && lastTriggered >= maxDate) ||
     createdAt >= maxDate
@@ -91,11 +106,19 @@ async function run() {
   const connection = await createConnection();
   const alertRepository = connection.getRepository(Alert);
 
-  const alerts = await alertRepository.find();
-  console.info(`Processing ${alerts.length} alerts.`);
+  const alerts = await alertRepository.find({ where: { active: true } });
+  console.info(`Processing ${alerts.length} active alerts.`);
 
-  await Promise.all(
-    alerts.map(async (alert) => processAlert(alert, alertRepository)),
+  await Bluebird.map(
+    alerts,
+    async (alert) => {
+      try {
+        await processAlert(alert, alertRepository);
+      } catch (error) {
+        console.error(`Error processing alert ${alert.id}:`, error);
+      }
+    },
+    { concurrency: 5 },
   );
 }
 
