9946a63553720e09dff6e1f8aab4d28ee9ee412a
Eric Boucher <eric.p.boucher@gmail.com>
2023-11-22 19:00:16 +0100

    Update LayerDownloadOptions.tsx

--- frontend/src/components/MapView/LeftPanel/layersPanel/MenuSwitch/SwitchItem/LayerDownloadOptions.tsx
+++ frontend/src/components/MapView/LeftPanel/layersPanel/MenuSwitch/SwitchItem/LayerDownloadOptions.tsx
@@ -36,6 +36,7 @@ import { isExposureAnalysisLoadingSelector } from 'context/analysisResultStateSl
 import { availableDatesSelector } from 'context/serverStateSlice';
 import { getRequestDate } from 'utils/server-utils';
 
+// TODO - return early when the layer is not selected.
 function LayerDownloadOptions({
   layer,
   extent,
