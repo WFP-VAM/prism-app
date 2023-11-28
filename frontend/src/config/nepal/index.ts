686debe5decefd7dd33d5e96369935398ffda93d
Amit Wadhwa <awadhwa@gmail.com>
2023-11-28 06:25:15 -0800

    Add initial configuration for prism Nepal; COUNTRY=nepal (#990)
    Co-authored-by: Eric Boucher <eric.p.boucher@gmail.com>

---
+++ frontend/src/config/nepal/index.ts
@@ -0,0 +1,15 @@
+import appConfig from './prism.json';
+import rawLayers from './layers.json';
+
+const rawTables = {};
+const translation = {};
+const rawReports = {};
+
+export default {
+  appConfig,
+  rawLayers,
+  rawTables,
+  rawReports,
+  translation,
+  defaultBoundariesFile: 'npl_admbnda_adm2_nd_20190430_dv.json',
+};
