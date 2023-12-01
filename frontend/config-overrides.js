6e4fe1b0d661ade260931b3377b17513cadb70b3
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-12-01 10:55:38 +0200

    Trim commit hash and add link to github

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -11,9 +11,10 @@ const RemovePlugin = require('remove-files-webpack-plugin');
 const country = process.env.REACT_APP_COUNTRY;
 
 // In case GIT_HASH is not set we are in github actions environment
-process.env.REACT_APP_GIT_HASH = process.env.GITHUB_SHA
+process.env.REACT_APP_GIT_HASH = (process.env.GITHUB_SHA
   ? process.env.GITHUB_SHA
-  : process.env.GIT_HASH;
+  : process.env.GIT_HASH
+)?.slice(0, 8);
 
 module.exports = override(
   useEslintRc(path.resolve(__dirname, '.eslintrc')),
--- frontend/src/components/Common/HashText/index.tsx
+++ frontend/src/components/Common/HashText/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const HashText = () => (
-  <p
-    style={{
-      position: 'absolute',
-      bottom: '0',
-      left: '1rem',
-    }}
-  >
-    web hash: {JSON.stringify(process.env.REACT_APP_GIT_HASH)}
-  </p>
-);
+const HashText = () => {
+  const hash = process.env.REACT_APP_GIT_HASH;
+  return (
+    <p
+      style={{
+        color: '#f5f7f8',
+        position: 'absolute',
+        bottom: '0',
+        fontSize: '0.7rem',
+        left: '1rem',
+      }}
+    >
+      version hash:{' '}
+      {hash && (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://github.com/WFP-VAM/prism-app/tree/${hash}`}
+        >
+          {hash}
+        </a>
+      )}
+    </p>
+  );
+};
 
 export default HashText;
