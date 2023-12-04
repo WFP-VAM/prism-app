75d211c32876158ba27b4b11bf494b832d2a016e
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-12-04 16:11:07 +0200

    Make all text clickable

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -11,9 +11,8 @@ const RemovePlugin = require('remove-files-webpack-plugin');
 const country = process.env.REACT_APP_COUNTRY;
 
 // In case GIT_HASH is not set we are in github actions environment
-process.env.REACT_APP_GIT_HASH = (process.env.GITHUB_SHA
-  ? process.env.GITHUB_SHA
-  : process.env.GIT_HASH
+process.env.REACT_APP_GIT_HASH = (
+  process.env.GITHUB_SHA || process.env.GIT_HASH
 )?.slice(0, 8);
 
 module.exports = override(
--- frontend/src/components/Common/HashText/index.tsx
+++ frontend/src/components/Common/HashText/index.tsx
@@ -1,28 +1,27 @@
+// This component creates a hidden link, to the version in which the app was built.
 import React from 'react';
 
 const HashText = () => {
   const hash = process.env.REACT_APP_GIT_HASH;
   return (
-    <p
+    <a
       style={{
         color: '#f5f7f8',
         position: 'absolute',
-        bottom: '0',
+        bottom: '1rem',
         fontSize: '0.7rem',
         left: '1rem',
       }}
+      target="_blank"
+      rel="noopener noreferrer"
+      href={
+        hash
+          ? `https://github.com/WFP-VAM/prism-app/tree/${hash}`
+          : 'https://github.com/WFP-VAM/prism-app'
+      }
     >
-      version hash:{' '}
-      {hash && (
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://github.com/WFP-VAM/prism-app/tree/${hash}`}
-        >
-          {hash}
-        </a>
-      )}
-    </p>
+      version hash: {hash}
+    </a>
   );
 };
 
