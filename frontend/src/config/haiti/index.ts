1bdf021bddca93237ed8393df8bd633502a42789
Amit W <awadhwa@gmail.com>
2023-12-20 21:44:57 -0800

    initial config for haiti

---
+++ frontend/src/config/haiti/index.ts
@@ -0,0 +1,15 @@
+import appConfig from './prism.json';
+import rawLayers from './layers.json';
+
+const rawTables = {};
+const rawReports = {};
+const translation = {};
+
+export default {
+  appConfig,
+  rawLayers,
+  rawTables,
+  rawReports,
+  translation,
+  defaultBoundariesFile: 'hti_admbnda_adm2_cnigs_20181129_dv.json',
+};
