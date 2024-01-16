7f5ff28b1d3c0a38bb15774a725586179fac056c
Eric Boucher <eric.p.boucher@gmail.com>
2024-01-16 22:31:37 +0100

    Update server-utils.ts

--- frontend/src/utils/server-utils.ts
+++ frontend/src/utils/server-utils.ts
@@ -215,7 +215,7 @@ const getStaticRasterDataCoverage = (layer: StaticRasterLayerProps) => {
  * @return DateItem
  */
 const generateDefaultDateItem = (date: number, baseItem?: Object): DateItem => {
-  const newDate = moment(date).set({ hour: 12, minute: 0 }).valueOf()
+  const newDate = moment(date).set({ hour: 12, minute: 0 }).valueOf();
   const r = {
     displayDate: newDate,
     queryDate: newDate,
