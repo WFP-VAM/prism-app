b0a50e7a4fe7c594d9c74f2ccc05cb3234de10bd
Eric Boucher <eric.p.boucher@gmail.com>
2024-02-08 23:08:15 +0100

    Create getStacBand utility function

--- frontend/src/components/MapView/LeftPanel/layersPanel/MenuItem/MenuSwitch/SwitchItem/LayerDownloadOptions.tsx
+++ frontend/src/components/MapView/LeftPanel/layersPanel/MenuItem/MenuSwitch/SwitchItem/LayerDownloadOptions.tsx
@@ -35,7 +35,7 @@ import { useSafeTranslation } from 'i18n';
 import { isExposureAnalysisLoadingSelector } from 'context/analysisResultStateSlice';
 import { availableDatesSelector } from 'context/serverStateSlice';
 import { getRequestDate } from 'utils/server-utils';
-import { LayerDefinitions } from 'config/utils';
+import { LayerDefinitions, getStacBand } from 'config/utils';
 import { safeCountry } from 'config';
 
 // TODO - return early when the layer is not selected.
@@ -128,12 +128,7 @@ function LayerDownloadOptions({
 
   const handleDownloadGeoTiff = (): void => {
     const { serverLayerName, additionalQueryParams } = layer as WMSLayerProps;
-    // TODO - figure out a way to leverage style to guess band?
-    const { band } =
-      (additionalQueryParams as {
-        styles?: string;
-        band?: string;
-      }) || {};
+    const band = getStacBand(additionalQueryParams);
 
     setIsGeotiffLoading(true);
     const dateString = moment(selectedDate).format(DEFAULT_DATE_FORMAT);
