16d416c50586d14b6e9f60cf9a52a9defd6ffafe
Eric Boucher <eric.p.boucher@gmail.com>
2024-02-29 17:17:52 +0100

    Update index.tsx

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -216,7 +216,7 @@ const styles = (theme: Theme) =>
     },
     appBar: {
       backgroundImage: `linear-gradient(180deg, ${theme.palette.primary.main} 0%, ${theme.palette.primary.dark} 100%)`,
-      height: '7vh',
+      height: '6vh',
       display: 'flex',
       justifyContent: 'center',
     },
