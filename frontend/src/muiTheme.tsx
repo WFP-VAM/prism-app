3986050cc19580ee4a3828ccd51a2fd69fab9d49
Eleftherios Chaidemenos <echaidemenos@programize.com>
2024-03-07 12:14:47 +0200

    Remove un used code

--- frontend/src/muiTheme.tsx
+++ frontend/src/muiTheme.tsx
@@ -225,7 +225,6 @@ const theme: any = createTheme({
       root: {
         '&.Mui-disabled': {
           pointerEvents: 'auto',
-          color: '#282b2d',
         },
         borderRadius: '4px',
       },
