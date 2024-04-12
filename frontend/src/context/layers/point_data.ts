63378475d57f2e6df00baf1088fb61e6a9f451db
Eric Boucher <eric.p.boucher@gmail.com>
2024-04-12 16:37:03 +0200

    Update point_data.ts

--- frontend/src/context/layers/point_data.ts
+++ frontend/src/context/layers/point_data.ts
@@ -137,10 +137,9 @@ export const fetchPointLayerData: LazyLoader<PointDataLayerProps> = () => async 
       getState,
     });
   }
+  // If the data is already in the correct format, return it.
   if ((data as any)?.type === 'FeatureCollection') {
     return (data as any) as PointLayerData;
   }
-  return (GeoJSON.parse(data, {
-    Point: ['lat', 'lon'],
-  }) as unknown) as PointLayerData;
+  return GeoJSON.parse(data, { Point: ['lat', 'lon'] });
 };
