e2b2bdc88b9f4f27bb012f2221c675815ee0fd93
Amit W <awadhwa@gmail.com>
2024-06-06 09:57:43 -0700

    add initial config

---
+++ frontend/src/config/somalia/index.ts
@@ -0,0 +1,15 @@
+import appConfig from './prism.json';
+import rawLayers from './layers.json';
+
+const rawTables = {};
+const rawReports = {};
+const translation = {};
+
+export default {
+  appConfig,
+  rawLayers,
+  rawTables,
+  rawReports,
+  translation,
+  defaultBoundariesFile: 'som_admbnda_adm2_ocha_20230308_dv.json',
+};
