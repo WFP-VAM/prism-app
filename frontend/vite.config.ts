5ef1a4e58451b408b71c5317344c9b77b8a2ad1c
ericboucher <eric.p.boucher@gmail.com>
2024-07-05 13:05:27 +0200

    Remove files for other countries

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 // Load environment variables that start with REACT_APP_
 dotenv.config({ path: '.env' });
@@ -12,9 +14,51 @@ const env = Object.keys(process.env)
     return obj;
   }, {} as { [key: string]: string | undefined });
 
+const country = process.env.REACT_APP_COUNTRY || 'mozambique';
+if (!!country) {
+  console.log(
+    `Building for country ${country}. Removing data for other countries.`,
+  );
+}
+
+// In case GIT_HASH is not set we are in github actions environment
+process.env.REACT_APP_GIT_HASH = (
+  process.env.GITHUB_SHA || process.env.GIT_HASH
+)?.slice(0, 8);
+
+// Custom plugin to remove files
+const removeFilesPlugin = (): Plugin => {
+  return {
+    name: 'vite-plugin-remove-files',
+    closeBundle() {
+      const root = path.resolve(__dirname, 'build', 'data');
+      const regex = new RegExp(country.toLowerCase(), 'm');
+
+      const removeFiles = (dir: string) => {
+        fs.readdirSync(dir).forEach(file => {
+          const absPath = path.join(dir, file);
+          if (fs.statSync(absPath).isDirectory()) {
+            removeFiles(absPath);
+          } else if (!regex.test(absPath)) {
+            fs.unlinkSync(absPath);
+          }
+        });
+      };
+
+      if (fs.existsSync(root)) {
+        removeFiles(root);
+      }
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(),
+    nodePolyfills(),
+    removeFilesPlugin(),
+  ],
   define: {
     'process.env': env,
   },
