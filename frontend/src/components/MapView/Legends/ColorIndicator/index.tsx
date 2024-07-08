464c09bcb3153cf404d7a789e3ec7b4d361bbbd3
Eleftherios Chaidemenos <echaidemenos@programize.com>
2024-07-08 14:32:32 +0300

    Update tests

--- frontend/src/components/MapView/Legends/ColorIndicator/index.tsx
+++ frontend/src/components/MapView/Legends/ColorIndicator/index.tsx
@@ -1,18 +1,6 @@
 import { createStyles, makeStyles, Typography } from '@material-ui/core';
 import { alpha } from '@material-ui/core/styles';
 
-interface StylesProps {
-  color: string;
-  opacity: number;
-  fillPattern: 'left' | 'right' | undefined;
-}
-
-const indicatorsCommonStyles = {
-  height: 10,
-  width: 10,
-  marginRight: 4,
-};
-
 const useStyles = makeStyles(() =>
   createStyles({
     container: {
@@ -20,23 +8,9 @@ const useStyles = makeStyles(() =>
       alignItems: 'center',
     },
     indicator: {
-      ...indicatorsCommonStyles,
-      backgroundColor: ({ color, opacity }: StylesProps) =>
-        alpha(color, opacity),
-    },
-    fillPatternIndicator: {
-      ...indicatorsCommonStyles,
-      background: ({ color, fillPattern }: StylesProps) =>
-        `linear-gradient(
-          to ${fillPattern} bottom, 
-          ${color} calc(100% / 7),  
-          white calc(100% / 7) calc(100% / 7 * 2), 
-          ${color} calc(100% / 7 * 2) calc(100% / 7 * 3), 
-          white calc(100% / 7 * 3) calc(100% / 7 * 4), 
-          ${color} calc(100% / 7 * 4) calc(100% / 7 * 5),
-          white calc(100% / 7 * 5) calc(100% / 7 * 6), 
-          ${color} calc(100% / 7 * 6)
-        )`,
+      height: 10,
+      width: 10,
+      marginRight: 4,
     },
   }),
 );
@@ -47,14 +21,26 @@ function ColorIndicator({
   opacity,
   fillPattern,
 }: ColorIndicatorProps) {
-  const classes = useStyles({ color, opacity, fillPattern });
+  const classes = useStyles();
 
   return (
     <div className={classes.container}>
       <div
-        className={
-          fillPattern ? classes.fillPatternIndicator : classes.indicator
-        }
+        className={classes.indicator}
+        style={{
+          backgroundColor: fillPattern
+            ? `linear-gradient(
+          to ${fillPattern} bottom, 
+          ${color} calc(100% / 7),  
+          white calc(100% / 7) calc(100% / 7 * 2), 
+          ${color} calc(100% / 7 * 2) calc(100% / 7 * 3), 
+          white calc(100% / 7 * 3) calc(100% / 7 * 4), 
+          ${color} calc(100% / 7 * 4) calc(100% / 7 * 5),
+          white calc(100% / 7 * 5) calc(100% / 7 * 6), 
+          ${color} calc(100% / 7 * 6)
+        )`
+            : alpha(color, opacity),
+        }}
       />
       <Typography color="textSecondary">{value}</Typography>
     </div>
