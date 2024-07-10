211e12960b4bf1d71cbc9ea7844d4dff058fa393
ericboucher <eric.p.boucher@gmail.com>
2024-07-10 16:00:57 +0200

    Update index.tsx

--- frontend/src/components/MapView/AlertForm/index.tsx
+++ frontend/src/components/MapView/AlertForm/index.tsx
@@ -32,8 +32,7 @@ import { ALERT_API_URL } from 'utils/constants';
 
 // Not fully RFC-compliant, but should filter out obviously-invalid emails.
 // Source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-const EMAIL_REGEX: RegExp =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const EMAIL_REGEX: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // This should probably be determined on a case-by-case basis,
 // depending on if the downstream API has the capability.
@@ -111,35 +110,36 @@ function AlertForm({ classes, isOpen, setOpen }: AlertFormProps) {
     setEmail(newEmail);
   };
 
-  const onOptionChange =
-    <T extends string>(setterFunc: Dispatch<SetStateAction<T>>) =>
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const value = event.target.value as T;
-      setterFunc(value);
-      return value;
-    };
+  const onOptionChange = <T extends string>(
+    setterFunc: Dispatch<SetStateAction<T>>,
+  ) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value as T;
+    setterFunc(value);
+    return value;
+  };
 
   // specially for threshold values, also does error checking
   const onThresholdOptionChange = useCallback(
-    (thresholdType: 'above' | 'below') =>
-      (event: React.ChangeEvent<HTMLInputElement>) => {
-        const setterFunc =
-          thresholdType === 'above' ? setAboveThreshold : setBelowThreshold;
-        const changedOption = onOptionChange(setterFunc)(event);
-        // setting a value doesn't update the existing value until next render,
-        // therefore we must decide whether to access the old one or the newly change one here.
-        const aboveThresholdValue = parseFloat(
-          thresholdType === 'above' ? changedOption : aboveThreshold,
-        );
-        const belowThresholdValue = parseFloat(
-          thresholdType === 'below' ? changedOption : belowThreshold,
-        );
-        if (belowThresholdValue > aboveThresholdValue) {
-          setThresholdError('Below threshold is larger than above threshold!');
-        } else {
-          setThresholdError(null);
-        }
-      },
+    (thresholdType: 'above' | 'below') => (
+      event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+      const setterFunc =
+        thresholdType === 'above' ? setAboveThreshold : setBelowThreshold;
+      const changedOption = onOptionChange(setterFunc)(event);
+      // setting a value doesn't update the existing value until next render,
+      // therefore we must decide whether to access the old one or the newly change one here.
+      const aboveThresholdValue = parseFloat(
+        thresholdType === 'above' ? changedOption : aboveThreshold,
+      );
+      const belowThresholdValue = parseFloat(
+        thresholdType === 'below' ? changedOption : belowThreshold,
+      );
+      if (belowThresholdValue > aboveThresholdValue) {
+        setThresholdError('Below threshold is larger than above threshold!');
+      } else {
+        setThresholdError(null);
+      }
+    },
     [aboveThreshold, belowThreshold],
   );
 
