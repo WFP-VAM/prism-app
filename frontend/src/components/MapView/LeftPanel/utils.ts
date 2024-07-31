5bd595b8979322fd342ec093de718bb5a47f1319
Will Gislason <8203830+gislawill@users.noreply.github.com>
2024-07-31 11:19:03 -0700

    Return layer with group from formatLayersCategories regardless of whether layer is frozen

--- frontend/src/components/MapView/LeftPanel/utils.ts
+++ frontend/src/components/MapView/LeftPanel/utils.ts
@@ -50,7 +50,7 @@ function formatLayersCategories(
           layer.group = group;
         }
 
-        return layer;
+        return { ...layer, group };
       }
       return LayerDefinitions[key as LayerKey];
     }),
