27fd6c71231a89b602a109115ddd9197395bbbf2
ericboucher <eric.p.boucher@gmail.com>
2024-08-13 14:59:10 +0200

    log request

--- alerting/src/utils/analysis-utils.ts
+++ alerting/src/utils/analysis-utils.ts
@@ -116,6 +116,8 @@ export async function calculateBoundsForAlert(date: Date, alert: Alert) {
     zones: alert.zones,
   };
 
+  console.log(apiRequest);
+
   try {
     const apiData = await fetchApiData(`${API_URL}/stats`, apiRequest);
     return apiData && getAlertMessage(apiData, alert);
