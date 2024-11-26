6489a7d6f382387cd9c372d805d2da5c0ddb0dd6
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-11-26 16:47:31 +0100

    isolate AA drought specific legen items, setup empty legend for AA storm

---
+++ frontend/src/components/MapView/LeftPanel/AnticipatoryActionPanel/AAStormLegend/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@material-ui/core';
+
+function AAStormLegend() {
+  return (
+    <Typography>
+      AA storm Legend : nothing specific to AA storm to show yet
+    </Typography>
+  );
+}
+
+export default AAStormLegend;
