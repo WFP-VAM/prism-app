116f28ffda8ca8a0066bb2d0065c98ebc5822246
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-12-06 11:30:19 +0100

    isolate landfall popup content to be able to test it

--- frontend/src/components/MapView/Layers/AnticipatoryActionStormLayer/AAStormLandfallPopup/index.tsx
+++ frontend/src/components/MapView/Layers/AnticipatoryActionStormLayer/AAStormLandfallPopup/index.tsx
@@ -1,12 +1,13 @@
-import { createStyles, makeStyles, Typography } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core';
 import { Point } from 'geojson';
 import { Popup } from 'react-map-gl/maplibre';
+import PopupContent, { LandfallInfo } from './PopupContent';
 
 function AAStormLandfallPopup({
-  reportDate,
   point,
-  landfallInfo,
   onClose,
+  landfallInfo,
+  reportDate,
 }: AAStormLandfallPopupProps) {
   const classes = useStyles();
 
@@ -25,59 +26,11 @@ function AAStormLandfallPopup({
       className={classes.popup}
       maxWidth="280px"
     >
-      <div className={classes.itemsContainer}>
-        <Typography
-          variant="body1"
-          className={`${classes.text} ${classes.title}`}
-        >
-          Report date: {reportDate}
-        </Typography>
-        <div className={classes.itemContainer}>
-          <Typography variant="body1" className={classes.text}>
-            landfall <span className={classes.block}>estimated time</span>
-          </Typography>
-          <Typography
-            variant="body1"
-            className={`${classes.text} ${classes.textAlignRight}`}
-          >
-            12-03-2025
-            <span className={classes.block}>06:00 - 12:00</span>
-          </Typography>
-        </div>
-        <div className={classes.itemContainer}>
-          <Typography variant="body1" className={classes.text}>
-            landfall estimated
-            <span className={classes.block}>leadtime</span>
-          </Typography>
-          <Typography
-            variant="body1"
-            className={`${classes.text} ${classes.textAlignRight}`}
-          >
-            8 - 12 hrs
-          </Typography>
-        </div>
-        <div className={classes.itemContainer}>
-          <Typography variant="body1" className={classes.text}>
-            District impacted
-            <span className={classes.block}>by landfall</span>
-          </Typography>
-          <Typography
-            variant="body1"
-            className={`${classes.text} ${classes.textAlignRight}`}
-          >
-            {landfallInfo.landfall_impact_district}
-          </Typography>
-        </div>
-      </div>
+      <PopupContent landfallInfo={landfallInfo} reportDate={reportDate} />
     </Popup>
   );
 }
 
-interface LandfallInfo {
-  landfall_time: string[];
-  landfall_impact_district: string;
-  landfall_impact_intensity: string[];
-}
 interface AAStormLandfallPopupProps {
   point: Point;
   landfallInfo: LandfallInfo;
@@ -100,32 +53,7 @@ const useStyles = makeStyles(() =>
         borderRightWidth: '8px',
       },
     },
-    block: {
-      display: 'block',
-    },
-    itemsContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '2px',
-    },
-    itemContainer: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      padding: '8px',
-      background: '#FFFFFF',
-    },
-    text: {
-      fontSize: '14px',
-      lineHeight: '18px',
-      fontWeight: 400,
-    },
-    title: {
-      fontWeight: 700,
-      padding: '2px 0px 2px 8px',
-    },
-    textAlignRight: {
-      textAlign: 'right',
-    },
   }),
 );
+
 export default AAStormLandfallPopup;
