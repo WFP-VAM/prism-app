6866f5419764ba6fc4cb3cc7fef439ba8fd6ff1a
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-12-09 18:21:51 +0100

    fix failing test

--- frontend/src/components/MapView/Layers/AnticipatoryActionStormLayer/AAStormLandfallPopup/PopupContent/index.test.tsx
+++ frontend/src/components/MapView/Layers/AnticipatoryActionStormLayer/AAStormLandfallPopup/PopupContent/index.test.tsx
@@ -3,7 +3,7 @@ import PopupContent from '.';
 
 describe('AAStormLandfallPopup component', () => {
   it('renders as expected', () => {
-    const reportDate = '2024-03-01 6p.m.';
+    const reportDate = '2024-03-01 18:00:00';
     const landfallInfo = {
       landfall_time: ['2024-03-12 00:00:00', '2024-03-12 06:00:00'],
       landfall_impact_district: 'Inhassoro',
