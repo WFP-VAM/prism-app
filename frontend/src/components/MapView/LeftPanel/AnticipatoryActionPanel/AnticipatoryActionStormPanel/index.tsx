5521743eb4d842927bad7546e8056caac6799444
donia benharara <donia.benharara@pathtech.fr>
2024-12-11 12:12:09 +0100

    clean storm state

--- frontend/src/components/MapView/LeftPanel/AnticipatoryActionPanel/AnticipatoryActionStormPanel/index.tsx
+++ frontend/src/components/MapView/LeftPanel/AnticipatoryActionPanel/AnticipatoryActionStormPanel/index.tsx
@@ -1,33 +1,14 @@
 import { Typography, createStyles, makeStyles } from '@material-ui/core';
 import React from 'react';
 import { useSafeTranslation } from 'i18n';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  AAAvailableDatesSelector,
-  setAAFilters,
-} from 'context/anticipatoryAction/AAStormStateSlice';
-import { dateRangeSelector } from 'context/mapStateSlice/selectors';
-import { getRequestDate } from 'utils/server-utils';
-import { getFormattedDate } from 'utils/date-utils';
-import { DateFormat } from 'utils/name-utils';
 import { PanelSize } from 'config/types';
 import HowToReadModal from '../HowToReadModal';
 import ActivationTrigger from './ActivationTriggerView';
 
 function AnticipatoryActionStormPanel() {
   const classes = useStyles();
-  const dispatch = useDispatch();
   const { t } = useSafeTranslation();
-  const AAAvailableDates = useSelector(AAAvailableDatesSelector);
-  const { startDate: selectedDate } = useSelector(dateRangeSelector);
   const [howToReadModalOpen, setHowToReadModalOpen] = React.useState(false);
-
-  const queryDate = getRequestDate(AAAvailableDates, selectedDate);
-  const date = getFormattedDate(queryDate, DateFormat.Default) as string;
-
-  React.useEffect(() => {
-    dispatch(setAAFilters({ selectedDate: date }));
-  }, [date, dispatch]);
 
   return (
     <div
