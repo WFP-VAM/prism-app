2880c7b14433a1420c182f9efb1bfef5b7e2ee54
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2025-01-09 15:28:35 +0100

    remove duplicated types and enum

--- frontend/src/context/anticipatoryAction/AAStormStateSlice/types.ts
+++ frontend/src/context/anticipatoryAction/AAStormStateSlice/types.ts
@@ -70,8 +70,8 @@ export enum WindState {
   activated_64 = 'activated_64',
   activated_118 = 'activated_118',
 }
-type ShortDate = string;
-type CycloneName = string;
+export type ShortDate = string;
+export type CycloneName = string;
 export type TimeAndState = { ref_time: string; state: WindState };
 export type AAStormWindStateReports = Record<
   ShortDate,
