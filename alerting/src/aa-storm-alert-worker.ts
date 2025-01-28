e69bccd946eff9bb73ca5ab929e5c701806916cb
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2025-01-28 16:52:03 +0100

    basic version for the email logic

--- alerting/src/aa-storm-alert-worker.ts
+++ alerting/src/aa-storm-alert-worker.ts
@@ -7,19 +7,154 @@ import { Alert } from './entities/alerts.entity';
 import { calculateAlert } from './utils/analysis-utils';
 import { sendEmail } from './utils/email';
 import { fetchCoverageLayerDays, formatUrl, WMS } from 'prism-common';
+import moment from 'moment';
+import { StormDataResponseBody, WindState } from './types/rawStormDataTypes';
+
+interface EmailPayload {}
+
+interface ShortReport {
+  ref_time: string;
+  state: string;
+  path: string;
+}
+
+interface ShortReportsResponseBody {
+  [date: string]: {
+    [stormName: string]: ShortReport[];
+  };
+}
 
 // @ts-ignore
 global.fetch = nodeFetch;
 
-function shouldSendEmail(): boolean {
+function fetchAllReports(): Promise<ShortReportsResponseBody | null> {
+  try {
+    return fetch(
+      'https://data.earthobservation.vam.wfp.org/public-share/aa/ts/outputs/dates.json',
+    ).then((data) => data.json());
+  } catch {
+    return Promise.reject(null);
+  }
+}
+
+function isEmailNeededByReport(report: StormDataResponseBody) {
+  if (report.landfall_detected) {
+    return false;
+  }
+
+  const status = report.ready_set_results?.status;
+
+  if (!status) {
+    return false;
+  }
+
+  if (status === WindState.ready) {
+    return true;
+  }
+
+  const exposed_area_64kt = report.ready_set_results?.exposed_area_64kt;
+
+  const watchedDistrictsFor64KtStorm = [
+    'Mogincual',
+    'Namacurra',
+    'Cidade Da Beira',
+    'Buzi',
+    'Dondo',
+    'Vilankulo',
+  ];
+
+  if (
+    status === WindState.activated_118 &&
+    exposed_area_64kt &&
+    exposed_area_64kt.affected_districts.filter((district) =>
+      watchedDistrictsFor64KtStorm.includes(district),
+    ).length > 0
+  ) {
+    return true;
+  }
+
+  const exposed_area_48kt = report.ready_set_results?.exposed_area_48kt;
+
+  const watchedDistrictsFor48ktStorm = [
+    'Angoche',
+    'Maganja Da Costa',
+    'Machanga',
+    'Govuro',
+  ];
+
+  if (
+    status === WindState.activated_64 &&
+    exposed_area_48kt &&
+    exposed_area_48kt.affected_districts.filter((district) =>
+      watchedDistrictsFor48ktStorm.includes(district),
+    ).length > 0
+  ) {
+    return true;
+  }
+
   return false;
+}
+
+async function buildEmailPayloads(): Promise<EmailPayload[]> {
+  // get all the reports
+  const allReports = await fetchAllReports();
+  console.log('allReports', allReports);
+  if (!allReports) {
+    console.error('Error fetching all reports');
+    return [];
+  }
+
+  // filter today's reports
+  const today = moment.utc().format('YYYY-MM-DD');
+  const todaysReports = allReports[today];
+  if (!todaysReports) {
+    return [];
+  }
+
+  // for each today's storm
+  const falsyOrEmailPayloads = await Promise.all(
+    Object.keys(todaysReports).map(async (stormName) => {
+      const stormShortReports = todaysReports[stormName];
+
+      // get the latest detailed report for that storm
+      const latestReport = stormShortReports.reduce(
+        (latestShortReport, currentShortReport) =>
+          new Date(currentShortReport.ref_time) >
+          new Date(latestShortReport.ref_time)
+            ? currentShortReport
+            : latestShortReport,
+      );
+
+      const detailedStormReport: StormDataResponseBody = await fetch(
+        `https://data.earthobservation.vam.wfp.org/public-share/aa/ts/outputs/${latestReport.path}?v2`,
+      ).then((data) => data.json());
+
+      console.log('detailedstormreport', detailedStormReport);
+      // decide whether an email is required
+      const isEmailNeeded = isEmailNeededByReport(detailedStormReport);
+
+      if (isEmailNeeded) {
+        // TODO: add the missing items required to feed the email template
+        return {
+          cycloneName: detailedStormReport.forecast_details.cyclone_name,
+        };
+      }
+
+      return false;
+    }),
+  );
+
+  return falsyOrEmailPayloads.filter((payload) => payload);
 }
 
 async function run() {
   //check whether an email should be sent
-  if (shouldSendEmail()) {
-    // send email
-  }
+  buildEmailPayloads().then((emailPayloads) => {
+    console.log('emailPayload', emailPayloads);
+
+    // create templates
+    // send emails
+  });
 }
 
 run();
---
+++ alerting/src/types/rawStormDataTypes.ts
@@ -0,0 +1,79 @@
+//TODO: Avoid duplication of code
+// the code below is duplicated from /home/max/projets/prism-app/frontend/src/context/anticipatoryAction/AAStormStateSlice/types.ts
+export enum WindState {
+  monitoring = 'monitoring',
+  ready = 'ready',
+  activated_64 = 'activated_64', // TODO: should be renamed as activated_48 ie activated_48 = 'activated_64' so to always use the kt unit everywhere
+  activated_118 = 'activated_118', // TODO: should be renamed as activated_64 ie activated_64 = 'activated_118' so to always use the kt unit everywhere
+}
+
+//TODO: Avoid duplication of code
+// the code below is duplicated from /prism-app/frontend/src/context/anticipatoryAction/AAStormStateSlice/rawStormDataTypes.ts.
+import { FeatureCollection, Point, Feature } from 'geojson';
+
+export enum FeaturePropertyDataType {
+  analysis = 'analysis',
+  forecast = 'forecast',
+}
+
+interface FeatureProperties {
+  time: string;
+  [key: string]: any;
+}
+
+/* timeserie types */
+
+export interface TimeSerieFeatureProperty {
+  data_type: FeaturePropertyDataType;
+  time: string;
+  development: string;
+  // maximum_wind_speed: number;
+  // maximum_wind_gust: number;
+  // wind_buffer_48: any;
+  // wind_buffer_64: any;
+}
+
+export interface TimeSeries
+  extends FeatureCollection<Point, TimeSerieFeatureProperty> {}
+export interface AAStormTimeSeriesFeature
+  extends Feature<Point, TimeSerieFeatureProperty> {}
+
+/* forcast-details types */
+export interface ForecastDetails {
+  basin: string;
+  cyclone_name: string;
+  event_id: string;
+  reference_time: string;
+  season: number;
+}
+
+/* landfall-info types */
+export enum AACategoryLandfall {
+  Severe = 'severe tropical storm',
+  Moderate = 'moderate tropical storm',
+}
+
+/* ready_set_results types */
+interface ExposedAreaStorm {
+  affected_districts: string[];
+  polygon: any;
+}
+
+/* storm data reponse body type */
+export interface StormDataResponseBody {
+  time_series: TimeSeries;
+  landfall_detected: boolean;
+  forecast_details: ForecastDetails;
+  uncertainty_cone: FeatureProperties;
+  landfall_info: {
+    landfall_time: string[];
+    landfall_impact_district: string;
+    landfall_impact_intensity: AACategoryLandfall[];
+  };
+  ready_set_results?: {
+    status: WindState;
+    exposed_area_48kt: ExposedAreaStorm;
+    exposed_area_64kt: ExposedAreaStorm;
+    proba_48kt_20_5d: ExposedAreaStorm;
+  };
+}
