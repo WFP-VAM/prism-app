0b2baa6dcc4c112ac039047ae66708479a4e3834
donia benharara <donia.benharara@pathtech.fr>
2025-02-18 15:34:52 +0100

    fix lint

--- common/src/index.ts
+++ common/src/index.ts
@@ -5,18 +5,20 @@ export { WCS, createGetCoverageUrl } from "./wcs";
 export { formatUrl } from "./utils";
 
 export type {
-  TimeSerieFeatureProperty,
-  TimeSeries,
-  AAStormTimeSeriesFeature,
-  ForecastDetails,
-  LandfallInfo,
-  StormDataResponseBody,
-} from "./types/anticipatory-action-storm/reportResponse";
-export {
-  FeaturePropertyDataType,
-  AACategoryLandfall,
-} from "./types/anticipatory-action-storm/reportResponse";
-export {
-  WindState,
-  displayWindState,
-} from "./types/anticipatory-action-storm/windState";
+    TimeSerieFeatureProperty,
+    TimeSeries,
+    AAStormTimeSeriesFeature,
+    ForecastDetails,
+    LandfallInfo,
+    StormDataResponseBody,
+  } from "./types/anticipatory-action-storm/reportResponse";
+  
+  export {
+    FeaturePropertyDataType,
+    AACategoryLandfall,
+  } from "./types/anticipatory-action-storm/reportResponse";
+  
+  export {
+    WindState,
+    displayWindState,
+  } from "./types/anticipatory-action-storm/windState";
