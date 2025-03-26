99334c09e409ecaf0d976f5770dfccbc2f49e024
Amit W <awadhwa@gmail.com>
2025-03-25 22:41:12 -0700

    initial configuration for malawi

---
+++ frontend/src/config/malawi/index.ts
@@ -0,0 +1,15 @@
+import appConfig from './prism.json';
+import rawLayers from './layers.json';
+
+const rawTables = {};
+const rawReports = {};
+const translation = {};
+
+export default {
+  appConfig,
+  rawLayers,
+  rawTables,
+  rawReports,
+  translation,
+  defaultBoundariesFile: 'mwi_admbnda_adm2_nso_hotosm_20230405_dv.json',
+};
