0ab8846794cd82583db3dcc35ab14351066adca2
Will Gislason <8203830+gislawill@users.noreply.github.com>
2025-04-15 09:01:42 -0700

    Updating PRISM auth

--- api/app/duckdb_utils.py
+++ api/app/duckdb_utils.py
@@ -17,7 +17,6 @@ def setup_duckdb_connection():
                 TYPE S3,
                 KEY_ID '{os.environ["VECTOR_STORE_ACCESS_KEY_ID"]}',
                 SECRET '{os.environ["VECTOR_STORE_SECRET_ACCESS_KEY"]}',
-                {f"SESSION_TOKEN '{os.environ['VECTOR_STORE_SESSION_TOKEN']}'," if os.environ.get("VECTOR_STORE_SESSION_TOKEN") else ""}
                 REGION '{os.environ["VECTOR_STORE_DEFAULT_REGION"]}'
             );
         """
