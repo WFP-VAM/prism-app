0ce0bb73af642312538fabb5a763c6a845caeb49
Laurent Savaëte <laurent@where.tf>
2025-08-13 18:24:51 +0200

    Debug ci error

---
+++ frontend/cypress/support/component.tsx
@@ -0,0 +1,41 @@
+// ***********************************************************
+// This example support/component.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+import { Provider } from 'react-redux'
+import { getStore } from '../../src/store'
+import { mount } from 'cypress/react'
+
+// Augment the Cypress namespace to include type definitions for
+// your custom command.
+// Alternatively, can be defined in cypress/support/component.d.ts
+// with a <reference path="./component" /> at the top of your spec.
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount
+    }
+  }
+}
+
+Cypress.Commands.add('mount', (component, options = {}) => {
+  // Use the default store if one is not provided
+  const { reduxStore = getStore(), ...mountOptions } = options;
+
+  const wrapped = <Provider store={ reduxStore }> { component } < /Provider>;
+
+  return mount(wrapped, mountOptions)
+})
