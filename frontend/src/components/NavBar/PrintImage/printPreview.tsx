7a872b18d974ef245766b2796368bd9f5e7165c3
Will Gislason <8203830+gislawill@users.noreply.github.com>
2025-10-22 12:56:55 -0500

    Pulling in hardcoded images via vite (#1556)
    * WIP load map and layers panel on Dashboard
    
    * Fix layer selection and date initialization issues
    
    * Set up MapBlock and update config types
    
    * Add basic support for legends in dashboard context
    
    * Update opacity logic to work with multiple maps
    
    * Rename `MapConfig` interface
    
    * Add support for config-driven default date
    
    * Add support for config-driven layer selection
    
    * Implement support for switching between multiple dashboards
    
    * Add support for multiple maps (and an example in the config)
    
    * Incorporate new date-loading logic
    
    * Add support for config-driven bounding boxes
    
    * Update styles
    
    - Brings padding more inline with Figma designs
    - Adjusts bounding box padding to account for legends
    - Adds height to map for a better aspect ratio in single-map view
    
    * Add fallback to config initialization
    
    * Update snapshots
    
    * Wrap strings in translate function
    
    * Extract duplicated switch statement to helper
    
    * Add basic support for Map preview mode
    
    * Refactor DahsboardView to support both edit and preview modes
    
    * Further consolidate edit/preview modes
    
    Removes standalone DashboardPreview component
    
    * Update title positioning in preview mode
    
    * Simplify mode state and default to "preview"
    
    * Allow legend to fill map height
    
    * TS fix
    
    * Analytics tables edit mode
    
    * Typescript fixes
    
    * Polish
    
    * Clear existing analysis state
    
    * Clean up comments
    
    * Leveraging new analysis hooks in Analysis Panel
    
    * Remove dashboard from admin boundary path
    
    * Adding translations to tableblock
    
    * Wrap strings in `t`
    
    * Fix test
    
    * Adding translation to line in BaselineLayerSelector
    
    * translate block name
    
    * Edit & Preview modes for charts in Dashboards
    
    * Using DashboardMode throughout DashboardView components
    
    * Fix chart title
    
    * Use chart hooks to reduce duplication
    
    * Seeting inputs to black in theme and cleaning up past one offs
    
    * WIP: export to pdf and png (layout in export not right yet)
    
    * Test fixes and performance improvement (don't keep dialog mounted)
    
    * Adding logo configurations
    
    * Updated snapshot
    
    * Make label black as well
    
    * Updating to support legend control
    
    * Updating snapshot
    
    * spacing update
    
    * Fixing admin label + filter toggles
    
    * Simplifying download, only supporting a4
    
    * Saving edit mode map bounds for use in print, updating snapshot
    
    * Fixing content heights
    
    * Update snapshot
    
    * Merge conflicts
    
    * Fixing test, another result of merge
    
    * Update prism.json
    
    * Update index.test.tsx.snap
    
    * Update path config for malai and somalia
    
    * Pulling in hardcoded images via vite
    
    * Test updates
    
    ---------
    
    Co-authored-by: Conor Henningson <sextonc@me.com>
    Co-authored-by: ericboucher <eric.p.boucher@gmail.com>

--- frontend/src/components/NavBar/PrintImage/printPreview.tsx
+++ frontend/src/components/NavBar/PrintImage/printPreview.tsx
@@ -14,6 +14,7 @@ import { Panel, AdminLevelDataLayerProps } from 'config/types';
 import useLayers from 'utils/layers-utils';
 import { addFillPatternImagesInMap } from 'components/MapView/Layers/AdminLevelDataLayer/utils';
 import { mapStyle } from 'components/MapView/Map/utils';
+import iconNorthArrow from 'public/images/icon_north_arrow.png';
 import {
   dateRangeSelector,
   mapSelector,
@@ -152,7 +153,7 @@ function PrintPreview() {
                 bottom: `${(footerHeight as number) + 40}px`,
                 right: '10px',
               }}
-              src="./images/icon_north_arrow.png"
+              src={iconNorthArrow}
               alt="northArrow"
             />
             {titleText && (
