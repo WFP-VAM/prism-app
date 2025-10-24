47916190c28c3f206b67a617f1ce27a0aa0fa944
Amit W <awadhwa@gmail.com>
2025-10-24 14:43:12 -0400

    remove reports from moz index.ts

--- frontend/src/config/mozambique/index.ts
+++ frontend/src/config/mozambique/index.ts
@@ -1,7 +1,6 @@
 import appConfig from './prism.json';
 import rawLayers from './layers.json';
 import rawTables from './tables.json';
-import rawReports from './reports.json';
 
 const translation = { pt: {}, en: {} };
 
@@ -9,7 +8,6 @@ export default {
   appConfig,
   rawLayers,
   rawTables,
-  rawReports,
   translation,
   defaultBoundariesFile: 'moz_bnd_adm3_WFP.json',
 };
