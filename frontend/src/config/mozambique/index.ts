c686c8e5c6ef5e65774800f3ef00df058f122323
ericboucher <eric.p.boucher@gmail.com>
2025-10-24 21:37:32 +0200

    Update index.ts

--- frontend/src/config/mozambique/index.ts
+++ frontend/src/config/mozambique/index.ts
@@ -3,10 +3,12 @@ import rawLayers from './layers.json';
 import rawTables from './tables.json';
 
 const translation = { pt: {}, en: {} };
+const rawReports = {};
 
 export default {
   appConfig,
   rawLayers,
+  rawReports,
   rawTables,
   translation,
   defaultBoundariesFile: 'moz_bnd_adm3_WFP.json',
