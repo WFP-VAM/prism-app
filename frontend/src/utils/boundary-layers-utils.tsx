6c617f8fd162dfad1453b2cce1915f3021db80c1
Will Gislason <8203830+gislawill@users.noreply.github.com>
2025-10-26 18:05:12 -0500

    Updating snapshot, making isBoundaryLayer more specific

--- frontend/src/utils/boundary-layers-utils.tsx
+++ frontend/src/utils/boundary-layers-utils.tsx
@@ -1,9 +1,7 @@
 import { LayerType } from 'config/types';
 
 export const isBoundaryLayer = (layer: LayerType): boolean =>
-  layer.type === 'boundary' ||
-  layer.type === 'admin_level_data' ||
-  layer.id.includes('admin');
+  layer.type === 'boundary' || layer.id.includes('admin');
 
 export const getNonBoundaryLayers = (layers: LayerType[]): LayerType[] =>
   layers.filter(layer => !isBoundaryLayer(layer));
