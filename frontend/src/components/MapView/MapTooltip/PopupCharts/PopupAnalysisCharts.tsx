813d226ad834071e9b10aef32ca30cb02130f8ca
ericboucher <eric.p.boucher@gmail.com>
2025-11-08 15:45:57 +0100

    Update PopupAnalysisCharts.tsx

--- frontend/src/components/MapView/MapTooltip/PopupCharts/PopupAnalysisCharts.tsx
+++ frontend/src/components/MapView/MapTooltip/PopupCharts/PopupAnalysisCharts.tsx
@@ -91,7 +91,7 @@ function PopupAnalysisCharts({
   const features = map?.queryRenderedFeatures(undefined, { layers: [layerId] });
 
   const adminProperties =
-    data && layer?.format === 'geojson'
+    data && layer?.format !== 'pmtiles'
       ? getProperties(data as BoundaryLayerData, adminCode, adminSelectorKey)
       : (features?.find(f => f.properties?.[adminSelectorKey] === adminCode)
           ?.properties ?? null);
