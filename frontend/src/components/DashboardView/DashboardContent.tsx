6285634b9a17e402e754775194ee3cd2a730256e
Will Gislason <8203830+gislawill@users.noreply.github.com>
2025-11-09 17:16:08 -0600

    Make charts update effectively

--- frontend/src/components/DashboardView/DashboardContent.tsx
+++ frontend/src/components/DashboardView/DashboardContent.tsx
@@ -298,7 +298,7 @@ function DashboardContent({
       case DashboardElementType.CHART:
         return (
           <Box
-            key={`chart-${elementId}`}
+            key={`chart-${elementId}-${needsEqualHeight}`}
             style={
               mode === DashboardMode.DASHBOARD &&
               !exportConfig &&
