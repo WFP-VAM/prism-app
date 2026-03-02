531e03bae0d64dc7a714d0853d907eb2562c7094
ericboucher <eric.p.boucher@gmail.com>
2026-03-02 12:06:32 +0100

    Fix flaky e2e tests: replace MapTiler attribution check with maplibregl-canvas

--- frontend/cypress/e2e/mozambique.cy.ts
+++ frontend/cypress/e2e/mozambique.cy.ts
@@ -18,7 +18,7 @@ describe('Loading dates', () => {
   it('switching to AA from rainfall layer should load latest data', () => {
     cy.visit(`${frontendUrl}/?hazardLayerIds=rainfall_dekad&date=2025-09-01`);
 
-    cy.contains('MapTiler', { timeout: 60000 }).should('be.visible');
+    cy.get('.maplibregl-canvas', { timeout: 60000 }).should('exist');
     cy.get('.react-datepicker-wrapper button span', { timeout: 20000 }).then(
       span1 => {
         cy.wrap(span1)
