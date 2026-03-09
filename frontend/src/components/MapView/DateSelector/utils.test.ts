1b433251d1df6965cc1927c90e82dfa567612554
ericboucher <eric.p.boucher@gmail.com>
2026-03-09 12:23:25 +0100

    Update utils.test.ts

--- frontend/src/components/MapView/DateSelector/utils.test.ts
+++ frontend/src/components/MapView/DateSelector/utils.test.ts
@@ -1,4 +1,4 @@
-import * as DateSelectorUtils from './utils';
+import { getDefaultCompatibleDate } from './utils';
 
 const jan21 = new Date('2024-01-21T12:00:00Z').getTime();
 const feb1 = new Date('2024-02-01T12:00:00Z').getTime();
@@ -6,10 +6,6 @@ const feb11 = new Date('2024-02-11T12:00:00Z').getTime();
 
 describe('DateSelector helpers', () => {
   test('defaults to latest compatible date when no selected date exists', () => {
-    const getDefaultCompatibleDate = (DateSelectorUtils as any)
-      .getDefaultCompatibleDate;
-
-    expect(getDefaultCompatibleDate).toBeDefined();
     expect(getDefaultCompatibleDate([jan21, feb1, feb11], undefined)).toBe(
       feb11,
     );
