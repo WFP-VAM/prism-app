1feaae8336946b95ac4e204bd451310096e76164
Cameron McCallie <mccallie.cameron@gmail.com>
2026-05-27 17:34:31 -0700

    remove unnecessary tests

--- api/prism_app/tests/test_permission_codes.py
+++
@@ -1,8 +0,0 @@
-"""Permission-code catalog invariants."""
-
-from prism_app.auth.permission_codes import ALL_CAPABILITIES, MAP_EXPORTS_MANAGE
-
-
-def test_map_exports_manage_permission_is_in_dev_capability_set() -> None:
-    assert MAP_EXPORTS_MANAGE == "prism.map_exports.manage"
-    assert MAP_EXPORTS_MANAGE in ALL_CAPABILITIES
