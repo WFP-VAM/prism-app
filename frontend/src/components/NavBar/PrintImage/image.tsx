48981914742d2071ee9a1f1a040c30e48239a41d
Eric Boucher <eric.p.boucher@gmail.com>
2026-06-09 11:48:10 +0200

    Improve admin area handling for scheduled maps (#1930)

--- frontend/src/components/NavBar/PrintImage/image.tsx
+++ frontend/src/components/NavBar/PrintImage/image.tsx
@@ -29,6 +29,10 @@ import React, {
 } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
+import {
+  filterFeaturesBySelectedAdminCodes,
+  resolveAdminAreaRefs,
+} from 'utils/adminAreaSelection';
 import { isBoundaryLayer } from 'utils/boundary-layers-utils';
 import { dateWithoutTime, getFormattedDate } from 'utils/date-utils';
 import useLayers, { isWmsSelectableForBatchPrint } from 'utils/layers-utils';
@@ -423,6 +427,11 @@ function DownloadImage({ open, handleClose }: DownloadImageProps) {
     }
   }, [availableCadences, cadence, createScheduledMaps, disabledCadences]);
 
+  const adminAreaRefs = useMemo(
+    () => resolveAdminAreaRefs(selectedBoundaries, data, boundaryLayer, i18n),
+    [data, selectedBoundaries, i18n],
+  );
+
   const mapExportTemplate = useMapExportTemplate({
     mapBounds: previewBounds,
     mapDimensions,
@@ -438,6 +447,7 @@ function DownloadImage({ open, handleClose }: DownloadImageProps) {
     bottomLogoScale,
     toggles,
     selectedBoundaries,
+    adminAreaRefs,
     language: exportLanguage(location.search, {
       activeLanguage: i18n.resolvedLanguage,
     }),
@@ -528,8 +538,10 @@ function DownloadImage({ open, handleClose }: DownloadImageProps) {
 
     const filteredData = data && {
       ...data,
-      features: data.features.filter(cell =>
-        selectedBoundaries.includes(cell.properties?.[boundaryLayer.adminCode]),
+      features: filterFeaturesBySelectedAdminCodes(
+        data.features,
+        boundaryLayer,
+        selectedBoundaries,
       ),
     };
     const masked = mask(filteredData as any);
