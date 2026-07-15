8f1d0feaf35a703da50e6463df4475ac98ab555d
Eric Boucher <eric.p.boucher@gmail.com>
2026-07-15 19:34:44 +0200

    Multiple admin level data fixes; COUNTRY=rbd (#2001)

--- frontend/src/utils/useDefaultDate.ts
+++ frontend/src/utils/useDefaultDate.ts
@@ -1,7 +1,7 @@
 import { isMainLayer, LayerKey, SelectedDateTimestamp } from 'config/types';
 import { availableDatesSelector } from 'context/serverStateSlice';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { getFormattedDate } from './date-utils';
 import { useUrlHistory } from './url-utils';
@@ -15,13 +15,13 @@ import { useMapState } from './useMapState';
 export function useDefaultDate(
   layerId: LayerKey,
 ): SelectedDateTimestamp | undefined {
-  const dispatch = useDispatch();
   const { dateRange, layers, ...mapState } = useMapState();
   // check layer without group or main layer in group
   const mainLayer = isMainLayer(layerId as string, layers);
   const { startDate: selectedDate } = dateRange;
 
-  const { updateHistory } = useUrlHistory();
+  const { urlParams, updateHistory } = useUrlHistory();
+  const urlDate = urlParams.get('date');
 
   // TODO - use getPossibleDatesForLayer
   const possibleDates = useSelector(availableDatesSelector)[layerId];
@@ -33,21 +33,22 @@ export function useDefaultDate(
   // React doesn't allow updating other components within another component
   // useEffect removes this error and updates DateSelector correctly in the lifecycle.
   useEffect(() => {
-    if (!selectedDate && defaultDate && mainLayer) {
-      // Update both URL and Redux state to ensure DateSelector re-renders properly
-      updateHistory('date', getFormattedDate(defaultDate, 'default') as string);
-      if (!mapState.isGlobalMap) {
-        mapState.actions.updateDateRange({ startDate: defaultDate });
-      }
+    if (urlDate || selectedDate || !defaultDate || !mainLayer) {
+      return;
+    }
+    // Update both URL and Redux state to ensure DateSelector re-renders properly
+    updateHistory('date', getFormattedDate(defaultDate, 'default') as string);
+    if (!mapState.isGlobalMap) {
+      mapState.actions.updateDateRange({ startDate: defaultDate });
     }
   }, [
     defaultDate,
-    dispatch,
     selectedDate,
     updateHistory,
     mainLayer,
     mapState.isGlobalMap,
     mapState.actions,
+    urlDate,
   ]);
 
   return (selectedDate as SelectedDateTimestamp) || defaultDate;
